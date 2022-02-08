166a111d0d107576df20897d54d7edb91aac8183
Dimitri Racordon <dimitri.racordon@gmail.com>
2022-02-08 12:10:52 -0500

    Refine the name mangler

--- Sources/VIL/Mangler.swift
+++ Sources/VIL/Mangler.swift
@@ -8,7 +8,7 @@ import AST
 public struct Mangler {
 
   /// A buffer accumulating the components of the name mangling.
-  private var buffer: String = Mangler.prefix
+  fileprivate var buffer: String = Mangler.prefix
 
   public init() {}
 
@@ -24,9 +24,7 @@ public struct Mangler {
         sanitized.append(Character(scalar))
       } else {
         let string = String(scalar.value, radix: 36)
-        sanitized.append("_")
-        sanitized.append(String(repeating: "0", count: 4 - string.count))
-        sanitized.append(string)
+        sanitized.append("_\(string.count)\(string)")
       }
     }
 
@@ -36,39 +34,32 @@ public struct Mangler {
 
   public mutating func append(index: Int) {
     assert(index > 0)
-    buffer.append(String(describing: index))
-    buffer.append("_")
+    buffer.append("\(index)_")
   }
 
   public mutating func append(space: DeclSpace) {
     switch space {
     case let decl as ModuleDecl:
       append(name: decl.name)
-      append(key: .moduleDecl)
 
     case let decl as ViewTypeDecl:
       append(space: decl.parentDeclSpace!)
       append(name: decl.name)
-      append(key: .viewDecl)
 
     case let decl as ProductTypeDecl:
       append(space: decl.parentDeclSpace!)
       append(name: decl.name)
-      append(key: .productDecl)
 
     case let decl as AliasTypeDecl:
       append(space: decl.parentDeclSpace!)
       append(name: decl.name)
-      append(key: .aliasDecl)
 
     case let decl as TypeExtnDecl:
       append(space: decl.parentDeclSpace!)
-      append(key: .extnDecl)
 
     case let decl as NamespaceDecl:
       append(space: decl.parentDeclSpace!)
       append(name: decl.name)
-      append(key: .namespaceDecl)
 
     case let decl as BaseFunDecl:
       append(funDecl: decl)
@@ -88,61 +79,37 @@ public struct Mangler {
     } else {
       append(name: funDecl.name)
     }
+    append(key: .ident)
     append(type: funDecl.type)
     append(key: .funDecl)
   }
 
   public mutating func append(witnessImpl impl: BaseFunDecl, for req: BaseFunDecl) {
     append(space: impl.parentDeclSpace!)
-    append(space: req.parentDeclSpace!)
     append(name: impl.name)
+    append(key: .ident)
+    append(space: req.parentDeclSpace!)
+    append(name: req.name)
+    append(key: .ident)
     append(type: req.type)
     append(key: .funDecl)
     append(key: .witnessImpl)
   }
 
   public mutating func append(type: ValType) {
-    switch type.dealiased {
-    case let tupleType as TupleType:
-      if tupleType == type.context.unitType {
-        append(key: .unitType)
-        break
-      }
+    type.accept(&self)
+  }
 
-      for elem in tupleType.elems {
-        if let label = elem.label {
-          append(name: label)
-          append(key: .tupleTypeLabel)
-        }
-        append(type: elem.type)
-        append(key: .tupleTypeElem)
-      }
-      append(key: .tupleType)
-
-    case let funType as FunType:
-      for param in funType.params {
-        if let label = param.label {
-          append(name: label)
-          append(key: .tupleTypeLabel)
-        }
-        append(type: param.type)
-        append(key: .tupleTypeElem)
-      }
-      append(key: .funTypeParam)
-      append(type: funType.retType)
-      append(key: .funType)
-
-    case is BuiltinIntLiteralType:
-      buffer.append("il")
-      append(key: .builtinType)
-
-    case let builtinType as BuiltinType:
-      buffer.append(builtinType.name)
-      append(key: .builtinType)
-
-    default:
-      break
+  public mutating func append(policy: PassingPolicy) {
+    switch policy {
+    case .local:
+      buffer.append("1l")
+    case .inout:
+      buffer.append("1i")
+    case .consuming:
+      buffer.append("1c")
     }
+    append(key: .passingPolicy)
   }
 
   public mutating func finalize() -> String {
@@ -160,25 +127,187 @@ public struct Mangler {
 
 }
 
+extension Mangler: TypeVisitor {
+
+  public typealias Result = Void
+
+  public mutating func visit(_ type: KindType) {
+    type.type.accept(&self)
+    append(key: .kindType)
+  }
+
+  public mutating func visit(_ type: BuiltinType) {
+    buffer.append(type.name)
+    append(key: .builtinType)
+  }
+
+  public mutating func visit(_ type: BuiltinPointerType) {
+    visit(type as BuiltinType)
+  }
+
+  public mutating func visit(_ type: BuiltinIntLiteralType) {
+    visit(type as BuiltinType)
+  }
+
+  public mutating func visit(_ type: BuiltinIntType) {
+    visit(type as BuiltinType)
+  }
+
+  public mutating func visit(_ type: ModuleType) {
+    append(name: type.module.name)
+    append(key: .moduleType)
+  }
+
+  public mutating func visit(_ type: NamespaceType) {
+    append(space: type.decl)
+    append(key: .namespaceType)
+  }
+
+  public mutating func visit(_ type: ProductType) {
+    append(space: type.decl)
+    append(key: .productType)
+  }
+
+  public mutating func visit(_ type: ViewType) {
+    append(space: type.decl)
+    append(key: .viewType)
+  }
+
+  public mutating func visit(_ type: AliasType) {
+    type.dealiased.accept(&self)
+  }
+
+  public mutating func visit(_ type: ViewCompositionType) {
+    for view in type.views {
+      append(type: view)
+    }
+    append(key: .viewCompType)
+  }
+
+  public mutating func visit(_ type: UnionType) {
+    for elem in type.elems {
+      append(type: elem)
+    }
+    append(key: .unitType)
+  }
+
+  public mutating func visit(_ type: BoundGenericType) {
+    append(space: type.decl)
+    append(key: .ident)
+    for arg in type.args {
+      append(type: arg)
+    }
+    append(key: .boundGenericType)
+  }
+
+  public mutating func visit(_ type: GenericParamType) {
+    append(name: type.decl.name)
+    append(key: .genericParamType)
+  }
+
+  public mutating func visit(_ type: AssocType) {
+    append(type: type.interface)
+    append(key: .assocType)
+  }
+
+  public mutating func visit(_ type: SkolemType) {
+    append(type: type.interface)
+    append(key: .skolemType)
+  }
+
+  public mutating func visit(_ type: WitnessType) {
+    fatalError("unreachable")
+  }
+
+  public mutating func visit(_ type: TupleType) {
+    for elem in type.elems {
+      if let label = elem.label {
+        append(name: label)
+        append(key: .tupleTypeLabel)
+      }
+      append(type: elem.type)
+      append(key: .tupleTypeElem)
+    }
+    append(key: .tupleType)
+  }
+
+  public mutating func visit(_ type: FunType) {
+    for param in type.params {
+      if let label = param.label {
+        append(name: label)
+        append(key: .tupleTypeLabel)
+      }
+      append(type: param.type)
+      append(key: .tupleTypeElem)
+    }
+    append(key: .tupleType)
+    append(type: type.retType)
+    append(key: .funType)
+  }
+
+  public mutating func visit(_ type: FunParamType) {
+    append(policy: type.policy)
+    append(type: type.rawType)
+    append(key: .funParamType)
+  }
+
+  public mutating func visit(_ type: AsyncType) {
+    type.base.accept(&self)
+    append(key: .asyncType)
+  }
+
+  public mutating func visit(_ type: UnresolvedType) {
+    append(key: .unresolvedType)
+  }
+
+  public mutating func visit(_ type: ErrorType) {
+    append(key: .errorType)
+  }
+
+  public mutating func visit(_ type: TypeVar) {
+    buffer.append(String(describing: type))
+  }
+
+}
+
 public enum ManglingOperatorKey: String {
 
-  case funDecl        = "F"
-  case moduleDecl     = "M"
-  case productDecl    = "P"
-  case viewDecl       = "V"
-  case aliasDecl      = "A"
-  case extnDecl       = "X"
-  case namespaceDecl  = "N"
+  case ident            = "I"
 
-  case witnessImpl    = "W"
+  case aliasDecl        = "A"
+  case funDecl          = "F"
+  case moduleDecl       = "M"
+  case namespaceDecl    = "N"
+  case productDecl      = "P"
+  case viewDecl         = "V"
+  case extnDecl         = "X"
 
-  case builtinType    = "b"
-  case tupleType      = "t"
-  case tupleTypeElem  = "tE"
-  case tupleTypeLabel = "tL"
-  case funType        = "f"
-  case funTypeParam   = "fP"
-  case unitType       = "u"
+  case witnessImpl      = "W"
+
+  case passingPolicy    = "L"
+
+  case assocType        = "a"
+  case builtinType      = "b"
+  case boundGenericType = "bG"
+  case errorType        = "e"
+  case funType          = "f"
+  case funParamType     = "fP"
+  case typeList         = "l"
+  case genericParamType = "g"
+  case kindType         = "k"
+  case moduleType       = "m"
+  case namespaceType    = "n"
+  case productType      = "p"
+  case skolemType       = "s"
+  case tupleType        = "t"
+  case tupleTypeElem    = "tE"
+  case tupleTypeLabel   = "tL"
+  case unionType        = "u"
+  case viewType         = "v"
+  case viewCompType     = "vC"
+  case unresolvedType   = "x"
+  case asyncType        = "y"
+  case unitType         = "z"
 
 }
 
