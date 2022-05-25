408f4a7792e640f8d3c8772a211344c107be1d4c
Dave Abrahams <dabrahams@adobe.com>
2022-05-25 10:37:28 -0700

    Reachability analysis

--- Sources/ParseGen/EBNFGrammar.swift
+++ Sources/ParseGen/EBNFGrammar.swift
@@ -33,32 +33,63 @@ extension EBNF.Grammar {
 
   func validate() throws {
     var errors: Set<Error> = []
+    var reachable: Set<Symbol> = []
 
     for d in definitions.values {
-      validate(d.alternatives)
+      checkDefined(d.alternatives)
     }
 
-    func validate(_ x: EBNF.Token) {
+    reach(start)
+
+    for d in definitions.values {
+      if !reachable.contains(d.lhs.text) {
+        errors.insert(
+          Error("Symbol '\(d.lhs.text)' is unreachable from start symbol '\(start.text)'",
+                at: d.lhs.position))
+      }
+    }
+
+    func reach(_ x: Token) {
+      if reachable.insert(x.text).inserted {
+        reach(definitions[x.text]!.alternatives)
+      }
+    }
+
+    func reach(_ x: EBNF.AlternativeList) {
+      for a in x {
+        for t in a { reach(t) }
+      }
+    }
+
+    func reach(_ x: Term) {
+      switch x {
+      case .group(let g): reach(g)
+      case .symbol(let s): reach(s)
+      case .regexp(_, _): do {}
+      case .literal(_, _): do {}
+      case .quantified(let t, _, _): reach(t)
+      }
+    }
+
+    func checkDefined(_ x: EBNF.Token) {
       if definitions[x.text] == nil {
         errors.insert(Error("undefined symbol '\(x.text)'", at: x.position))
       }
     }
 
-    func validate(_ x: EBNF.AlternativeList) {
-      for rhs in x { validate(rhs) }
+    func checkDefined(_ x: EBNF.AlternativeList) {
+      for a in x {
+        for t in a { checkDefined(t) }
+      }
     }
 
-    func validate(_ x: Alternative) {
-      for rhs in x { validate(rhs) }
-    }
-
-    func validate(_ x: Term) {
+    func checkDefined(_ x: Term) {
       switch x {
-      case .group(let g): validate(g)
-      case .symbol(let s): validate(s)
+      case .group(let g): checkDefined(g)
+      case .symbol(let s): checkDefined(s)
       case .regexp(_, _): do {}
       case .literal(_, _): do {}
-      case .quantified(let t, _, _): validate(t)
+      case .quantified(let t, _, _): checkDefined(t)
       }
     }
     if !errors.isEmpty { throw errors.sorted { $0.site.span.lowerBound < $1.site.span.lowerBound } }
