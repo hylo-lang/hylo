9f063d444da68ba9cd0d1ad4b2f8d8e35ba7a95a
Dave Abrahams <dabrahams@adobe.com>
2022-05-29 20:17:34 -0700

    Attempt to account for whitespace rules

--- Sources/ParseGen/ParserGenerator.swift
+++ Sources/ParseGen/ParserGenerator.swift
@@ -1,6 +1,21 @@
 import Marpa
 import CitronLexerModule
 
+extension Collection {
+  // TODO: Should return an efficient adapter
+  /// Returns the elements of `self` with `x` between each adjacent pair.
+  func interjecting(_ x: Element) -> [Element] {
+    var r: [Element] = []
+    var source = self[...]
+    if let first = source.popFirst() {
+      r.reserveCapacity(count * 2 - 1)
+      r.append(first)
+      r.append(contentsOf: source.lazy.flatMap { y in [x, y] })
+    }
+    return r
+  }
+}
+
 func makeParser(_ sourceGrammar: EBNF.Grammar) throws -> Parser {
   let g: Marpa.Grammar
 
@@ -25,11 +40,6 @@ func makeParser(_ sourceGrammar: EBNF.Grammar) throws -> Parser {
   }
 
   let unrecognizedToken = g.makeTerminal()
-  var tokenPatterns = Dictionary(
-    uniqueKeysWithValues: regexps.lazy.map { name, pattern in (pattern, symbols[name])})
-  // ignore whitespace and single-line comments
-  tokenPatterns[#"\s+|//.*\p{Zl}*"#, default: nil] = nil
-
 
   // make MARPA symbols for nonterminals
   let sortedNonterminals = sourceGrammar.nonterminals().sorted()
@@ -40,12 +50,15 @@ func makeParser(_ sourceGrammar: EBNF.Grammar) throws -> Parser {
 
   var symbolName = Dictionary(uniqueKeysWithValues: symbols.lazy.map { (k, v) in (v, k) })
   symbolName[unrecognizedToken] = "<UNRECOGNIZED>"
+  let whitespaceOpt = symbols["whitespace-opt"]!
+  let horizontalSpaceOpt = symbols["horizontal-space-opt"]!
 
   // make MARPA rules
   var ruleLocation: [Marpa.Rule: SourceRegion] = [:]
   for lhs in sortedNonterminals {
-    for rhs in sourceGrammar.definitions[lhs]!.alternatives {
-      makeRule(lhs: symbols[lhs]!, rhs: rhs)
+    let d = sourceGrammar.definitions[lhs]!
+    for rhs in d.alternatives {
+      makeRule(lhs: symbols[lhs]!, rhs: rhs, kind: d.kind)
     }
   }
 
@@ -66,18 +79,34 @@ func makeParser(_ sourceGrammar: EBNF.Grammar) throws -> Parser {
     throw errors
   }
 
-  /// Returns a new MARPA rule with the given LHS symbol and MARPA symbols corresponding to rhs.
-  func makeRule(lhs: Marpa.Symbol, rhs: EBNF.Grammar.Alternative) {
-    let r = g.makeRule(lhs: lhs, rhs: rhs.lazy.map { t in symbol(t, lhs: symbolName[lhs]!) })
-    ruleLocation[r] = rhs.position
+  /// Returns a new MARPA rule of the given `kind` with the given `lhs` symbol
+  /// and MARPA symbols corresponding to `rhs`.
+  func makeRule(lhs: Marpa.Symbol, rhs: EBNF.Grammar.Alternative, kind: EBNF.Definition.Kind) {
+    let basicRHS = rhs.lazy.map { t in symbol(t, lhs: symbolName[lhs]!, kind: kind) }
+    makeRule(lhs: lhs, rhs: basicRHS, kind: kind, origin: rhs.position)
+  }
+
+  /// Returns a new MARPA rule of the given `kind` with the given `lhs` and `rhs` symbol.
+  func makeRule<RHS: Collection>(
+    lhs: Marpa.Symbol, rhs basicRHS: RHS, kind: EBNF.Definition.Kind,
+    origin: SourceRegion
+  )
+    where RHS.Element == Marpa.Symbol
+  {
+    let r = kind == .noWhitespace
+      ? g.makeRule(lhs: lhs, rhs: basicRHS)
+      : g.makeRule(
+        lhs: lhs,
+        rhs: basicRHS.interjecting(kind == .noNewline ? horizontalSpaceOpt : whitespaceOpt))
+    ruleLocation[r] = origin
   }
 
   /// Returns a new nonterminal symbol with a unique name based on `root`.
-  func synthesizedNonterminal(_ root: EBNF.Grammar.Symbol) -> Marpa.Symbol {
+  func makeSymbol(_ root: EBNF.Grammar.Symbol, terminal: Bool = false) -> Marpa.Symbol {
     for suffix in 0...50 {
       let name = suffix == 0 ? root : "\(root)_\(suffix)"
       if symbols[name] == nil {
-        let r = g.makeNonterminal()
+        let r = terminal ? g.makeTerminal() : g.makeNonterminal()
         symbols[name] = r
         symbolName[r] = name
         return r
@@ -92,31 +121,38 @@ func makeParser(_ sourceGrammar: EBNF.Grammar) throws -> Parser {
     return "\(lhsName) -> \(rhs)"
   }
 
-  func symbol(_ x: EBNF.Grammar.Term, lhs: EBNF.Grammar.Symbol) -> Marpa.Symbol {
+  func symbol(_ x: EBNF.Grammar.Term, lhs: EBNF.Grammar.Symbol, kind: EBNF.Definition.Kind)
+    -> Marpa.Symbol
+  {
     if let r = astSymbols[x] { return r }
     switch x {
     case .group(let alternatives):
-      let innerLHS = synthesizedNonterminal(lhs)
+      let innerLHS = makeSymbol(lhs)
       astSymbols[x] = innerLHS
-      for rhs in alternatives { makeRule(lhs: innerLHS, rhs: rhs) }
+      for rhs in alternatives { makeRule(lhs: innerLHS, rhs: rhs, kind: kind) }
       return innerLHS
     case .symbol(let s): return symbols[s.text]!
     case .regexp(_, _): fatalError("unreachable")
     case .literal(let l, _): return literals[l]!
 
     case .quantified(let t, let q, _):
-      let ts = symbol(t, lhs: lhs)
-      let innerLHS = synthesizedNonterminal(symbolName[ts]! + (q == "?" ? "-opt" : "-list"))
-      func makeRule<R: Collection>(rhs: R) where R.Element == Marpa.Symbol {
-        let r = g.makeRule(lhs: innerLHS, rhs: rhs)
-        ruleLocation[r] = x.position
+      let t1 = symbol(t, lhs: lhs, kind: kind)
+      let innerLHS = makeSymbol(symbolName[t1]! + (q == "?" ? "-opt" : "-list"))
+      if q == "*" || q == "?" {
+        makeRule(lhs: innerLHS, rhs: EmptyCollection(), kind: kind, origin: x.position)
       }
-      if q == "*" || q == "?" { makeRule(rhs: EmptyCollection()) }
-      if q == "+" || q == "?" { makeRule(rhs: CollectionOfOne(ts)) }
-      if q == "*" || q == "+" { makeRule(rhs: [innerLHS, ts]) }
+      if q == "+" || q == "?" {
+        makeRule(lhs: innerLHS, rhs: CollectionOfOne(t1), kind: kind, origin: x.position)
+      }
+      if q == "*" || q == "+" {
+        makeRule(lhs: innerLHS, rhs: [innerLHS, t1], kind: kind, origin: x.position)
+      }
       return innerLHS
     }
   }
+
+  let tokenPatterns = Dictionary(
+    uniqueKeysWithValues: regexps.lazy.map { name, pattern in (pattern, symbols[name])})
 
   return Parser(
     grammar: g, unrecognizedToken: unrecognizedToken,
