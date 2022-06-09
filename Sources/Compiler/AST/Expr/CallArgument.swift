2b85da21fc19cefb7f8941264dc2cf93b5f32167
Dimitri Racordon <dimitri.racordon@gmail.com>
2022-06-09 10:22:33 -0400

    Remove `CallArgument.isInout`

--- Sources/Compiler/AST/Expr/CallArgument.swift
+++ Sources/Compiler/AST/Expr/CallArgument.swift
@@ -1,21 +1,13 @@
 /// An argument of a function or subscript call.
 public struct CallArgument: Hashable {
 
-  /// A flag indicating whether the argument is passed `inout`.
-  public var isInout: Bool
-
   /// The label of the argument, if any.
   public var label: SourceRepresentable<Identifier>?
 
   /// The value of the argument.
   public var value: AnyExprID
 
-  public init(
-    isInout: Bool = false,
-    label: SourceRepresentable<Identifier>? = nil,
-    value: AnyExprID
-  ) {
-    self.isInout = isInout
+  public init(label: SourceRepresentable<Identifier>? = nil, value: AnyExprID) {
     self.label = label
     self.value = value
   }
