89cc18ca68951213aa816a50f3a34a44bf2b9b41
Dimitri Racordon <dimitri.racordon@gmail.com>
2022-09-23 11:05:22 -0400

    Merge the representations of method and subscript introducers

---
+++ Sources/Compiler/AST/Decl/Implntroducer.swift
@@ -0,0 +1,22 @@
+/// The introducer of a method or subscript implementation.
+public enum ImplIntroducer: Codable {
+
+  case `let`
+
+  case `inout`
+
+  case set
+
+  case sink
+
+  /// The parameter passing convention corresponding to this introducer.
+  public var convention: PassingConvention {
+    switch self {
+    case .let   : return .let
+    case .inout : return .inout
+    case .set   : return .set
+    case .sink  : return .sink
+    }
+  }
+
+}
