6ee71807a810f1bb583d643b862c666303b623ed
Dimitri Racordon <dimitri.racordon@gmail.com>
2022-10-31 15:31:05 -0400

    Simplify the test driver setup

--- Tests/ValTests/TypeCheckerTests.swift
+++ Tests/ValTests/TypeCheckerTests.swift
@@ -5,60 +5,53 @@ import Utils
 final class TypeCheckerTests: XCTestCase {
 
   func testTypeChecker() throws {
+    // Locate the test cases.
+    let testCaseDirectory = try XCTUnwrap(
+      Bundle.module.url(forResource: "TestCases/TypeChecking", withExtension: nil),
+      "No test cases")
+
     // Prepare an AST with the core module loaded.
     var baseAST = AST()
     try baseAST.importValModule()
 
-    // Collect the URLs of the test cases for the type checker.
-    let urls = Bundle.module.urls(
-      forResourcesWithExtension: "val",
-      subdirectory: "TestCases/TypeChecking") ?? []
+    // Execute the test cases.
+    try TestCase.executeAll(in: testCaseDirectory, { (tc) in
+      // Create an AST for the test case.
+      var program = baseAST
 
-    for url in urls {
-      #if os(Linux)
-      let tc = try TestCase(source: SourceFile(contentsOf: URL(fileURLWithPath: url.path!)))
-      #else
-      let tc = try TestCase(source: SourceFile(contentsOf: url))
-      #endif
+      // Create a module for the input.
+      let module = program.insert(ModuleDecl(name: tc.name))
 
-      try tc.execute({ (source, annotations) in
-        // Create an AST for the test case.
-        var program = baseAST
+      // Parse the input.
+      if Parser.parse(tc.source, into: module, in: &program).decls == nil {
+        XCTFail("\(tc.name): parsing failed")
+        return
+      }
 
-        // Create a module for the input.
-        let module = program.insert(ModuleDecl(name: tc.name))
+      // Run the type checker.
+      var checker = TypeChecker(ast: program)
+      let success = checker.check(module: module)
 
-        // Parse the input.
-        if Parser.parse(source, into: module, in: &program).decls == nil {
-          XCTFail("\(tc.name): parsing failed")
-          return
+      // Create a diagnostic checker.
+      var diagnosticChecker = DiagnosticChecker(
+        testCaseName: tc.name, diagnostics: checker.diagnostics)
+
+      // Process the test annotations.
+      for annotation in tc.annotations {
+        switch annotation.command {
+        case "expect-failure":
+          XCTAssert(!success, "\(tc.name): type checking succeeded, but expected failure")
+        case "expect-success":
+          XCTAssert(success, "\(tc.name): type checking failed, but expected success")
+        case "diagnostic":
+          diagnosticChecker.handle(annotation)
+        default:
+          print("\(tc.name): unexpected test command: '\(annotation.command)'")
         }
+      }
 
-        // Run the type checker.
-        var checker = TypeChecker(ast: program)
-        let success = checker.check(module: module)
-
-        // Create a diagnostic checker.
-        var diagnosticChecker = DiagnosticChecker(
-          testCaseName: tc.name, diagnostics: checker.diagnostics)
-
-        // Process the test annotations.
-        for annotation in annotations {
-          switch annotation.command {
-          case "expect-failure":
-            XCTAssert(!success, "\(tc.name): type checking succeeded, but expected failure")
-          case "expect-success":
-            XCTAssert(success, "\(tc.name): type checking failed, but expected success")
-          case "diagnostic":
-            diagnosticChecker.handle(annotation)
-          default:
-            print("\(tc.name): unexpected test command: '\(annotation.command)'")
-          }
-        }
-
-        diagnosticChecker.finalize()
-      })
-    }
+      diagnosticChecker.finalize()
+    })
   }
 
 }
--- Tests/ValTests/Utils/TestCase.swift
+++ Tests/ValTests/Utils/TestCase.swift
@@ -1,5 +1,6 @@
 import Compiler
 import Foundation
+import Utils
 
 /// A helper for loading and executing test cases.
 struct TestCase {
@@ -7,14 +8,24 @@ struct TestCase {
   /// The source file defining the test case.
   var source: SourceFile
 
+  /// The test annotations of the test case.
+  var annotations: [TestAnnotation]
+
+  /// Loads a test case from `source`.
+  init(source: SourceFile) {
+    self.source = source
+    self.annotations = TestAnnotation.parseAll(from: source)
+  }
+
   /// The name of the test case.
   var name: String { source.url.deletingPathExtension().lastPathComponent }
 
-  /// Loads this test case and executes it with the specified handler.
-  ///
-  /// - Parameter handler: A closure that accepts a source file with a set of test annotations.
-  func execute(_ handler: (SourceFile, [TestAnnotation]) throws -> Void) throws {
-    try handler(source, TestAnnotation.parseAll(from: source))
+  /// Loads all the test cases in `directory` and executes them with the specified handler.
+  static func executeAll(in directory: URL, _ handler: (Self) throws -> Void) throws {
+    try withFiles(in: directory, { url in
+      try handler(TestCase(source: SourceFile(contentsOf: url)))
+      return true
+    })
   }
 
 }
