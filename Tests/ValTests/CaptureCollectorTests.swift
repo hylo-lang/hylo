18ff2a9cc96bf1c16885c2db683ced22da35a1ce
Dimitri Racordon <dimitri.racordon@gmail.com>
2022-11-01 18:47:31 -0400

    Replace handwritten AST

--- Tests/ValTests/CaptureCollectorTests.swift
+++ Tests/ValTests/CaptureCollectorTests.swift
@@ -4,86 +4,27 @@ import XCTest
 final class CaptureCollectorTests: XCTestCase {
 
   func testFunctionBindings() {
-
-    // fun f<X, @value v: Void>[let c = ()](_ p: Any) {
-    //   let _ = free   // captured
-    //   let _ = X      // bound
-    //   let _ = v      // bound
-    //   let _ = c      // bound
-    //   let _ = p      // bound
-    // }
-
     var ast = AST()
-    let fun = ast.insert(FunDecl(
-      introducer: SourceRepresentable(value: .fun),
-      identifier: SourceRepresentable(value: "f"),
-      genericClause: SourceRepresentable(value: GenericClause(
-        parameters: [
-          .type(ast.insert(GenericTypeParamDecl(
-            identifier: SourceRepresentable(value: "X")))),
-          .value(ast.insert(GenericValueParamDecl(
-            identifier: SourceRepresentable(value: "v"),
-            annotation: AnyTypeExprID(ast.insert(NameTypeExpr(
-              identifier: SourceRepresentable(value: "Void"), arguments: [])))))),
-        ])),
-      captures: [
-        ast.insert(BindingDecl(
-          pattern: ast.insert(BindingPattern(
-            introducer: SourceRepresentable(value: .let),
-            subpattern: AnyPatternID(ast.insert(NamePattern(
-              decl: ast.insert(VarDecl(
-                identifier: SourceRepresentable(value: "c")))))))),
-          initializer: AnyExprID(ast.insert(TupleExpr(elements: []))))),
-      ],
-      parameters: [
-        ast.insert(ParameterDecl(
-          identifier: SourceRepresentable(value: "p"),
-          annotation: ast.insert(ParameterTypeExpr(
-            convention: SourceRepresentable(value: .let),
-            bareType: AnyTypeExprID(ast.insert(NameTypeExpr(
-              identifier: SourceRepresentable(value: "Any"), arguments: []))))))),
-      ],
-      body: .block(ast.insert(BraceStmt(
-        stmts: [
-          AnyStmtID(ast.insert(DeclStmt(
-            decl: AnyDeclID(ast.insert(BindingDecl(
-              pattern: ast.insert(BindingPattern(
-                introducer: SourceRepresentable(value: .let),
-                subpattern: AnyPatternID(ast.insert(WildcardPattern())))),
-              initializer: AnyExprID(ast.insert(NameExpr(
-                name: SourceRepresentable(value: "free")))))))))),
-          AnyStmtID(ast.insert(DeclStmt(
-            decl: AnyDeclID(ast.insert(BindingDecl(
-              pattern: ast.insert(BindingPattern(
-                introducer: SourceRepresentable(value: .let),
-                subpattern: AnyPatternID(ast.insert(WildcardPattern())))),
-              initializer: AnyExprID(ast.insert(NameExpr(
-                name: SourceRepresentable(value: "X")))))))))),
-          AnyStmtID(ast.insert(DeclStmt(
-            decl: AnyDeclID(ast.insert(BindingDecl(
-              pattern: ast.insert(BindingPattern(
-                introducer: SourceRepresentable(value: .let),
-                subpattern: AnyPatternID(ast.insert(WildcardPattern())))),
-              initializer: AnyExprID(ast.insert(NameExpr(
-                name: SourceRepresentable(value: "v")))))))))),
-          AnyStmtID(ast.insert(DeclStmt(
-            decl: AnyDeclID(ast.insert(BindingDecl(
-              pattern: ast.insert(BindingPattern(
-                introducer: SourceRepresentable(value: .let),
-                subpattern: AnyPatternID(ast.insert(WildcardPattern())))),
-              initializer: AnyExprID(ast.insert(NameExpr(
-                name: SourceRepresentable(value: "c")))))))))),
-          AnyStmtID(ast.insert(DeclStmt(
-            decl: AnyDeclID(ast.insert(BindingDecl(
-              pattern: ast.insert(BindingPattern(
-                introducer: SourceRepresentable(value: .let),
-                subpattern: AnyPatternID(ast.insert(WildcardPattern())))),
-              initializer: AnyExprID(ast.insert(NameExpr(
-                name: SourceRepresentable(value: "p")))))))))),
-        ])))))
+    let module = ast.insert(ModuleDecl(name: "main"))
+    let source = SourceFile(contents: """
+      fun f<X, @value v: Void>[let c = ()](_ p: Any) {
+        let _ = free   // captured
+        let _ = X      // bound
+        let _ = v      // bound
+        let _ = c      // bound
+        let _ = p      // bound
+      }
+      """)
 
+    if Parser.parse(source, into: module, in: &ast).decls == nil {
+      XCTFail("parsing failed")
+      return
+    }
+
+    let fun = NodeID<FunDecl>(rawValue: ast.topLevelDecls(module).first!.rawValue)
     var collector = CaptureCollector(ast: ast)
     let captures = collector.freeNames(in: fun)
+
     XCTAssertEqual(captures.count, 2)
     XCTAssert(captures.keys.contains(Name(stem: "free")))
     XCTAssert(captures.keys.contains(Name(stem: "c")))
