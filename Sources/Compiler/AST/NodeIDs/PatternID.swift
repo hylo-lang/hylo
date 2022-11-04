994fd71b98db00b2e37d2449ce6e8d11360c11ec
Dimitri Racordon <dimitri.racordon@gmail.com>
2022-11-04 13:56:58 -0400

    Get rid of 'PatternVisitor'

--- Sources/Compiler/AST/NodeIDs/PatternID.swift
+++ Sources/Compiler/AST/NodeIDs/PatternID.swift
@@ -3,28 +3,6 @@ import Utils
 /// The ID of a pattern.
 public protocol PatternID: NodeIDProtocol {}
 
-extension PatternID {
-
-  /// Calls the `visitor.visit` method corresponding to the type of this node.
-  public func accept<V: PatternVisitor>(_ visitor: inout V) -> V.Result {
-    switch kind {
-    case .bindingPattern:
-      return visitor.visit(binding: NodeID(rawValue: rawValue))
-    case .exprPattern:
-      return visitor.visit(expr: NodeID(rawValue: rawValue))
-    case .namePattern:
-      return visitor.visit(name: NodeID(rawValue: rawValue))
-    case .tuplePattern:
-      return visitor.visit(tuple: NodeID(rawValue: rawValue))
-    case .wildcardPattern:
-      return visitor.visit(wildcard: NodeID(rawValue: rawValue))
-    default:
-      unreachable()
-    }
-  }
-
-}
-
 extension NodeID: PatternID where Subject: Pattern {}
 
 /// The type-erased ID of a pattern.
