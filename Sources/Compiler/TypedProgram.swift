1e3e738b8fa7de5682a0371a9c14e52d5fe1fc75
Dave Abrahams <dabrahams@adobe.com>
2022-11-09 15:59:53 -1000

    Wrapper WIP

--- Sources/Compiler/TypedProgram.swift
+++ Sources/Compiler/TypedProgram.swift
@@ -36,5 +36,87 @@ public struct TypedProgram: Program {
     self.exprTypes = exprTypes
     self.referredDecls = referredDecls
   }
+}
 
+public typealias ValNode = Node
+extension AST {
+  public typealias Node = ValNode
+}
+
+extension TypedProgram {
+  @dynamicMemberLookup
+  struct Node<ASTPart: AST.Node> {
+    let program: TypedProgram
+    let id: NodeID<ASTPart>
+
+    var astPart: ASTPart {
+      program.ast[id]
+    }
+
+    subscript<T>(dynamicMember m: KeyPath<ASTPart,T>) -> T {
+      astPart[keyPath: m]
+    }
+
+    subscript<T: AST.Node>(
+      dynamicMember m: KeyPath<ASTPart, [NodeID<T>]>
+    ) -> LazyMapCollection<[NodeID<T>], TypedProgram.Node<T>> {
+      astPart[keyPath: m].lazy.map { .init(program: program, id: $0) }
+    }
+
+    subscript<T: AST.Node>(
+      dynamicMember m: KeyPath<ASTPart, NodeID<T>?>
+    ) -> TypedProgram.Node<T>? {
+      astPart[keyPath: m].map { .init(program: program, id: $0) }
+    }
+  }
+}
+
+extension TypedProgram.Node where ASTPart: LexicalScope {
+  var parent: AnyScopeID? {
+    program.scopeToParent[id]
+  }
+
+  var decls: [AnyDeclID] {
+    program.scopeToDecls[id, default: []]
+  }
+}
+
+extension TypedProgram.Node where ASTPart: Decl {
+  var scope: AnyScopeID {
+    program.declToScope[id]!
+  }
+
+  var type: Type {
+    program.declTypes[id]!
+  }
+}
+
+extension TypedProgram.Node where ASTPart == VarDecl {
+  var binding: TypedProgram.Node<BindingDecl> {
+    .init(program: program, id: program.varToBinding[id]!)
+  }
+}
+
+extension TypedProgram.Node where ASTPart: Expr {
+  var type: Type {
+    program.exprTypes[id]!
+  }
+}
+
+extension TypedProgram.Node where ASTPart == NameExpr {
+  /// The declaration of this name.
+  var decl: DeclRef {
+    program.referredDecls[id]!
+  }
+}
+
+func crazyTest0(x: TypedProgram.Node<FunDecl>) -> [TypedProgram.Node<ParameterDecl>] {
+  print(x.type)
+  let p = x.parameters
+  return Array(p)
+}
+
+func crazyTest1(x: TypedProgram.Node<FunDecl>) -> TypedProgram.Node<ParameterDecl>? {
+  let p = x.implicitReceiverDecl
+  return p
 }
