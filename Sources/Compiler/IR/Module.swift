8cc33d6a25f728d8e53e57cc3b94e2d8d989bb18
Dimitri Racordon <dimitri.racordon@gmail.com>
2022-12-10 10:08:47 -0500

    Move 'ParameterType.asIRFunctionInput()' to its place

--- Sources/Compiler/IR/Module.swift
+++ Sources/Compiler/IR/Module.swift
@@ -180,19 +180,3 @@ extension Module {
   }
 
 }
-
-extension ParameterType {
-
-  /// Returns `self` as an input to an IR function.
-  func asIRFunctionInput() -> Function.Input {
-    switch convention {
-    case .let, .inout, .set:
-      return (convention: convention, type: .address(bareType))
-    case .sink:
-      return (convention: convention, type: .object(bareType))
-    case .yielded:
-      preconditionFailure("cannot lower yielded parameter")
-    }
-  }
-
-}
