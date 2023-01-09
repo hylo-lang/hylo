692ef0f3f69a777c37e40cafc70542221532ea8c
Dave Abrahams <dabrahams@adobe.com>
2023-01-09 14:20:00 -0800

    Use factored code to import core library

--- Sources/FrontEnd/AST+Extensions.swift
+++ Sources/FrontEnd/AST+Extensions.swift
@@ -10,27 +10,17 @@ extension AST {
   public mutating func importCoreModule() {
     precondition(!isCoreModuleLoaded, "Core library is already loaded")
 
-    var diagnostics = Diagnostics()
-    corelib = insert(ModuleDecl(name: "Val"), diagnostics: &diagnostics)
-    if !diagnostics.log.isEmpty {
-      fatalError("Error inserting the core module:\n\(diagnostics)")
+    do {
+      var diagnostics = Diagnostics()
+      corelib = try insert(
+        sourceFilesFromCommandPaths([ValModule.core!]).map(SourceFile.init),
+        asModule: "Val",
+        diagnostics: &diagnostics)
+
+      assert(isCoreModuleLoaded)
+    } catch let error {
+      fatalError("Error parsing the core module:\n\(error.localizedDescription)")
     }
-
-    withFiles(
-      in: ValModule.core!,
-      { (sourceURL) in
-        if sourceURL.pathExtension != "val" { return true }
-
-        // Parse the file.
-        do {
-          let sourceFile = try SourceFile(contentsOf: sourceURL)
-          _ = try Parser.parse(sourceFile, into: corelib!, in: &self, diagnostics: &diagnostics)
-          // Parsing the core module should generate no diagnostics, not even warnings.
-          if diagnostics.log.isEmpty { return true }
-          throw diagnostics
-        } catch let error {
-          fatalError("Error parsing the core module:\n\(error.localizedDescription)")
-        }
-      })
   }
+
 }
