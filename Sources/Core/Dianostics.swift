bd7ef60c05c964d7d488b67f1c62c87bf4d4a734
Dimi Racordon <dimitri.racordon@gmail.com>
2023-01-23 16:59:20 -0500

    Disable immediate diagnostic reporting

--- Sources/Core/Dianostics.swift
+++ Sources/Core/Dianostics.swift
@@ -1,11 +1,7 @@
 import Foundation
 
-/// A channel that accumulates reported `Diagnostic`s, optionally writing each unique one to
-/// standard error when it is reported.
+/// A channel that accumulates reported `Diagnostic`s.
 public struct Diagnostics {
-
-  /// Whether reported diagnostics are to be sent immediately to the standard error stream.
-  public let reportingToStderr: Bool
 
   /// All reported diagnostics.
   public private(set) var log: Set<Diagnostic> = []
@@ -13,22 +9,24 @@ public struct Diagnostics {
   /// Whether an error was reported.
   public private(set) var errorReported: Bool = false
 
-  /// Creates an instance that sends reported diagnostics to standard error iff `reportingToStderr`
-  /// is `true`.
-  ///
-  /// - Note: the default is `false` because we build many more tests than command-line tools.
-  public init(reportingToStderr: Bool = false) {
-    self.reportingToStderr = reportingToStderr
+  /// Creates an instance.
+  public init() {}
+
+  /// Creates an instance reporting `d`.
+  public init(_ d: Diagnostic) {
+    self = Diagnostics()
+    report(d)
+  }
+
+  /// Creates an instance reporting the elements of `batch`.
+  public init<B: Collection<Diagnostic>>(_ batch: B) {
+    report(batch)
   }
 
   /// Writes `d` into this diagnostic channel, setting `errorReported` iff `d` is an error.
   public mutating func report(_ d: Diagnostic) {
     if d.level == .error { errorReported = true }
-    if log.insert(d).inserted && reportingToStderr {
-      FileHandle.standardError.write(
-        // When lossy conversion is allowed, result is always non-nil
-        "\(d)".data(using: .utf8, allowLossyConversion: true)!)
-    }
+    log.insert(d)
   }
 
   /// Reports each diagnostic in `batch`.
@@ -39,18 +37,6 @@ public struct Diagnostics {
   /// Throws `self` if any errors were reported.
   public func throwOnError() throws {
     if errorReported { throw self }
-  }
-
-  /// Creates an instance reporting `d`.
-  public init(_ d: Diagnostic) {
-    self = Diagnostics()
-    report(d)
-  }
-
-  /// Creates an instance reporting the elements of `batch`.
-  public init<B: Collection<Diagnostic>>(_ batch: B, reportingToStderr: Bool = false) {
-    self = .init(reportingToStderr: reportingToStderr)
-    report(batch)
   }
 
 }
