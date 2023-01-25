ddd68122a8d3e73a4688d5d9478fff7e3a583130
Dimi Racordon <dimitri.racordon@gmail.com>
2023-01-25 13:57:20 -0500

    Synthesize a source site for 'BuiltinDecl'

--- Sources/Core/AST/Decl/BuiltinDecl.swift
+++ Sources/Core/AST/Decl/BuiltinDecl.swift
@@ -1,6 +1,11 @@
 /// An abstract node representing a built-in declaration.
 public struct BuiltinDecl: Decl {
 
-  public var site: SourceRange { .eliminateFIXME }
+  public let site: SourceRange
+
+  public init() {
+    let f = SourceFile(synthesizedText: "/* built-in declaration */")
+    self.site = f.range(f.text.startIndex ..< f.text.endIndex)
+  }
 
 }
