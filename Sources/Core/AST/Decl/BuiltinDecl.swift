df7134fa7a0dc6bc0b3c218eeb6cd1e3237c2bf7
Dimi Racordon <dimitri.racordon@gmail.com>
2023-01-26 08:39:44 -0500

    Make the source of a built-in decl a static constant

--- Sources/Core/AST/Decl/BuiltinDecl.swift
+++ Sources/Core/AST/Decl/BuiltinDecl.swift
@@ -4,8 +4,10 @@ public struct BuiltinDecl: Decl {
   public let site: SourceRange
 
   public init() {
-    let f = SourceFile(synthesizedText: "/* built-in declaration */")
-    self.site = f.range(f.text.startIndex ..< f.text.endIndex)
+    self.site = Self.source.range(Self.source.text.startIndex ..< Self.source.text.endIndex)
   }
+
+  /// A synthesized source file to define the site of this type's instances.
+  private static let source = SourceFile(synthesizedText: "/* built-in declaration */")
 
 }
