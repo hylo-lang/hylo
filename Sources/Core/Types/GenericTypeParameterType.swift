4b779bbc994c5068173b072cfe4c500543293bc9
Dimi Racordon <dimitri.racordon@gmail.com>
2023-02-10 14:18:10 -0500

    Remove unused initializer

--- Sources/Core/Types/GenericTypeParameterType.swift
+++ Sources/Core/Types/GenericTypeParameterType.swift
@@ -4,32 +4,20 @@ import Utils
 public struct GenericTypeParameterType: TypeProtocol {
 
   /// The declaration that introduces the parameter.
-  ///
-  /// - Note: The ID may denote the declaration of a generic type parameter or associated type.
-  public let decl: AnyDeclID
+  public let decl: NodeID<GenericParameterDecl>
 
   /// The name of the parameter.
   public let name: Incidental<String>
 
   /// Creates an instance denoting the generic type parameter declared by `decl`.
-  public init(_ decl: NodeID<AssociatedTypeDecl>, ast: AST) {
-    self.init(decl: AnyDeclID(decl), name: ast[decl].baseName)
-  }
-
-  /// Creates an instance denoting the generic type parameter declared by `decl`.
   public init(_ decl: NodeID<GenericParameterDecl>, ast: AST) {
-    self.init(decl: AnyDeclID(decl), name: ast[decl].baseName)
+    self.decl = decl
+    self.name = Incidental(ast[decl].baseName)
   }
 
   /// Creates an instance denoting the `Self` parameter of `trait`.
   public init(selfParameterOf trait: NodeID<TraitDecl>, in ast: AST) {
     self.init(ast[trait].selfParameterDecl, ast: ast)
-  }
-
-  /// Creates an instance denoting the generic type parameter declared by `decl`.
-  private init(decl: AnyDeclID, name: String) {
-    self.decl = decl
-    self.name = Incidental(name)
   }
 
   public var flags: TypeFlags { [.isCanonical, .hasGenericTypeParameter] }
