0d4480582bb827829719154e290db5ce9dc359de
Dimi Racordon <dimitri.racordon@gmail.com>
2023-02-11 16:04:28 -0500

    Fix non-portable code

--- Sources/Utils/Temporary.swift
+++ Sources/Utils/Temporary.swift
@@ -2,19 +2,9 @@ import Foundation
 
 extension FileManager {
 
-  /// Returns the URL of a temporary directory.
-  public func temporaryDirectory() throws -> URL {
-    try url(
-      for: .itemReplacementDirectory,
-      in: .userDomainMask,
-      appropriateFor: URL(
-        fileURLWithPath: FileManager.default.currentDirectoryPath, isDirectory: true),
-      create: true)
-  }
-
   /// Returns the URL of a temporary file.
-  public func temporaryFile() throws -> URL {
-    try temporaryDirectory().appendingPathComponent(ProcessInfo().globallyUniqueString)
+  public func temporaryFile() -> URL {
+    temporaryDirectory.appendingPathComponent(UUID().description)
   }
 
 }
