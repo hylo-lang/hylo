c6d0431d1cee23d6a39e37069c0dd97780bc5a0b
Dimi Racordon <dimitri.racordon@gmail.com>
2023-02-11 09:34:51 -0500

    Implement an abstraction representing checked conformances

---
+++ Sources/Core/Conformance.swift
@@ -0,0 +1,37 @@
+/// Where, how, and under what conditions a type satisfies the requirements of a trait.
+public struct Conformance {
+
+  /// A map from requirement to their implementation.
+  public typealias ImplementationMap = DeclProperty<AnyDeclID>
+
+  /// The type on the left-hand side of this conformance.
+  public let model: AnyType
+
+  /// The trait to which `model` conforms.
+  public let concept: TraitType
+
+  /// The conditions under which this conformance holds.
+  public let conditions: [Constraint]
+
+  /// The outermost scope in which this conformance holds.
+  public let scope: AnyScopeID
+
+  /// A map from requirement of `concept` to the declaration implementing it in `model`.
+  public let implementations: ImplementationMap
+
+  /// Creates an instance with the given properties.
+  public init(
+    model: AnyType,
+    concept: TraitType,
+    conditions: [Constraint],
+    scope: AnyScopeID,
+    implementations: ImplementationMap
+  ) {
+    self.model = model
+    self.concept = concept
+    self.conditions = conditions
+    self.scope = scope
+    self.implementations = implementations
+  }
+
+}
