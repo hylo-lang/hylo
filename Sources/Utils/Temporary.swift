bcf049d7a77481771d8fe57a9025fcc021faa3c7
Dimi Racordon <dimitri.racordon@gmail.com>
2023-02-11 16:00:47 -0500

    Remove unnecessary method

--- Sources/Utils/Temporary.swift
+++ Sources/Utils/Temporary.swift
@@ -2,19 +2,9 @@ import Foundation
 
 extension FileManager {
 
-  /// Returns the URL of a temporary directory.
-  public func temporaryDirectory() throws -> URL {
-    try url(
-      for: .itemReplacementDirectory,
-      in: .userDomainMask,
-      appropriateFor: URL(
-        fileURLWithPath: FileManager.default.currentDirectoryPath, isDirectory: true),
-      create: true)
-  }
-
   /// Returns the URL of a temporary file.
   public func temporaryFile() throws -> URL {
-    try temporaryDirectory().appendingPathComponent(ProcessInfo().globallyUniqueString)
+    temporaryDirectory.appendingPathComponent(ProcessInfo().globallyUniqueString)
   }
 
 }
