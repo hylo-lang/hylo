53f7038f96f09e9371191dc796f40d1b3591dace
Dimi Racordon <dimitri.racordon@gmail.com>
2023-02-15 09:42:38 -0500

    Use algorithms

--- Sources/ValCommand/SourceLine+Extensions.swift
+++ Sources/ValCommand/SourceLine+Extensions.swift
@@ -5,27 +5,13 @@ import Foundation
 extension SourceLine: ExpressibleByArgument {
 
   public init?(argument: String) {
-    var s = Substring(argument)
+    let x = argument.split(atLastIndexOf: ":")
+
     guard
-      let line = (s.takeSuffix(until: ":")?.dropFirst()).flatMap({ Int($0) }),
-      let source = try? SourceFile(contentsOf: URL(fileURLWithPath: String(s)))
+      let file = try? SourceFile(contentsOf: URL(fileURLWithPath: String(x.head))),
+      let line = Int(x.tail.dropFirst()).map(file.line(at:))
     else { return nil }
-
-    guard line > 0 && line <= source.line(containing: source.text.endIndex).number
-    else { return nil }
-
-    self = source.line(at: line)
-  }
-
-}
-
-extension Substring {
-
-  fileprivate mutating func takeSuffix(until ch: Character) -> Substring? {
-    guard let i = lastIndex(of: ch) else { return nil }
-    let result = suffix(from: i)
-    self = prefix(upTo: i)
-    return result
+    self = line
   }
 
 }
