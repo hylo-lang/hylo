1b21dae4a382c7800a566d4614f738a0632ff4aa
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2023-02-16 22:28:13 +0200

    Minor fixes.

--- Tests/EndToEndTests/ExecutionTests.swift
+++ Tests/EndToEndTests/ExecutionTests.swift
@@ -20,42 +20,12 @@ final class ExecutionTests: XCTestCase {
       })
   }
 
-  /// The result of a compiler invokation.
-  struct CompilationResult {
-
-    /// The exit status of the compiler.
-    let status: ExitCode
-
-    /// The URL of the output file.
-    let output: URL
-
-    /// The contents of the standard error.
-    let stderr: String
-
-    /// Check that the compilation was successful.
-    func assertSuccess(checkOutput: Bool = true) {
-      XCTAssert(status.isSuccess, "Compilation failed with exit code \(status)")
-      XCTAssert(stderr.isEmpty, "Compilation contains errors: \(stderr)")
-      if checkOutput {
-        XCTAssert(
-          FileManager.default.fileExists(atPath: output.relativePath),
-          "Compilation output file not found: \(output.relativePath)")
-      }
-    }
-
-  }
-
   /// Compiles `input` with the given arguments and returns the URL of the output file.
   ///
   /// Ensures that the compilation succeeds.
   func compile(_ input: URL, with arguments: [String]) throws -> URL {
-    // Create a temporary output.
     let output = FileManager.default.temporaryFile()
-
-    // Parse the command line's arguments.
     let cli = try ValCommand.parse(arguments + ["-o", output.relativePath, input.relativePath])
-
-    // Execute the command.
     var stderr = ""
     let status = try cli.execute(loggingTo: &stderr)
 
