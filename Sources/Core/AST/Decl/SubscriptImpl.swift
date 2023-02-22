01f0093330c5f6547a62054600535ed2eac37ccf
Dimi Racordon <dimitri.racordon@gmail.com>
2023-02-22 06:08:29 -0500

    Unify the representation of functions, methods, and subscript bodies

--- Sources/Core/AST/Decl/SubscriptImpl.swift
+++ Sources/Core/AST/Decl/SubscriptImpl.swift
@@ -3,27 +3,6 @@
 /// Instances of this type represent individual variant inside a subscript declaration.
 public struct SubscriptImpl: Decl, LexicalScope {
 
-  /// The body of a subscript implementation.
-  public enum Body: Codable {
-
-    /// An expression body.
-    case expr(AnyExprID)
-
-    /// A block body.
-    case block(BraceStmt.ID)
-
-    /// The node wrapped by this instance.
-    public var base: AnyNodeID {
-      switch self {
-      case .expr(let n):
-        return AnyNodeID(n)
-      case .block(let n):
-        return AnyNodeID(n)
-      }
-    }
-
-  }
-
   public let site: SourceRange
 
   /// The introducer of the subscript.
@@ -33,12 +12,12 @@ public struct SubscriptImpl: Decl, LexicalScope {
   public let receiver: ParameterDecl.ID?
 
   /// The body of the subscript, if any.
-  public let body: Body?
+  public let body: FunctionBody?
 
   public init(
     introducer: SourceRepresentable<AccessEffect>,
     receiver: ParameterDecl.ID?,
-    body: Body?,
+    body: FunctionBody?,
     site: SourceRange
   ) {
     self.site = site
