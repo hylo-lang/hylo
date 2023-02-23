fe395a5cb5136279cc50942797991a7b6b3a6c4e
Dimi Racordon <dimitri.racordon@gmail.com>
2023-02-23 17:26:41 -0500

    Fix typo

--- Sources/Core/Types/CallableType.swift
+++ Sources/Core/Types/CallableType.swift
@@ -1,7 +1,7 @@
 /// A type whose instances are callable entities.
 ///
-/// Do not declare new conformances to `CallableType`. Only the `LambdaType` and `MethodType` are
-/// are valid conforming types.
+/// Do not declare new conformances to `CallableType`. Only `LambdaType` and `MethodType` are valid
+/// conforming types.
 public protocol CallableType {
 
   /// The parameters of the callable value.
