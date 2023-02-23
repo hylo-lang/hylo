3777f5f946be4b905f831c309deac28093417a8c
Dimi Racordon <dimitri.racordon@gmail.com>
2023-02-23 10:03:14 -0500

    Implement 'Collection.rotate(_:toStartAt:)'

--- Tests/UtilsTests/CollectionExtensionsTests.swift
+++ Tests/UtilsTests/CollectionExtensionsTests.swift
@@ -41,3 +41,27 @@ final class BidirectionalCollectionExtensionsTests: XCTestCase {
   }
 
 }
+
+final class MutableCollectionExtensionsTests: XCTestCase {
+
+  func testRotateRange() {
+    var a = Array(0 ..< 7)
+    XCTAssertEqual(a.rotate(0 ..< 5, toStartAt: 3), 2)
+    XCTAssertEqual(a, [3, 4, 0, 1, 2, 5, 6])
+
+    let b = a
+    XCTAssertEqual(a.rotate(0 ..< 1, toStartAt: 0), 0)
+    XCTAssertEqual(a, b)
+  }
+
+  func testRotateWhole() {
+    var a = Array(0 ..< 7)
+    XCTAssertEqual(a.rotate(0 ..< 7, toStartAt: 4), 3)
+    XCTAssertEqual(a, [4, 5, 6, 0, 1, 2, 3])
+
+    let b = a
+    XCTAssertEqual(a.rotate(toStartAt: 0), 0)
+    XCTAssertEqual(a, b)
+  }
+
+}
