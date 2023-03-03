79300370557fbf05716b17ddfc01e4210db73d71
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2023-03-03 22:34:11 +0200

    Speed up execution of swift-format script.
    Find the executable, and call it directly for the swift source directories.

--- Tools/run-swift-format.sh
+++ Tools/run-swift-format.sh
@@ -10,18 +10,23 @@ TARGETS_ARGS="--target CLI --target ValCommand --target FrontEnd --target Core -
 
 
 function usage {
-    echo "Usage: $(basename $0) [-h] fix|lint"
+    echo "Usage: $(basename $0) [-h] [-d] fix|lint|fix_lint"
     echo "  -h          Display help"
+    echo "  -d          Use Debug swift-format builds (faster to compile swift-format)"
     echo "  fix         Run swift-format to correct formatting"
     echo "  lint        Run swift-format to check for formatting errors"
     echo "  fix_lint    Run swift-format to correct formatting and then check for formatting errors"
 }
 
-while getopts ":h" opt; do
+SWIFT_FORMAT_BUILD_MODE=release
+while getopts ":hd" opt; do
     case $opt in
         h)
             usage
             exit 0
+            ;;
+        d)
+            SWIFT_FORMAT_BUILD_MODE=debug
             ;;
         *)
             echo "Invalid parameters"
@@ -30,6 +35,7 @@ while getopts ":h" opt; do
             ;;
     esac
 done
+shift "$(($OPTIND -1))"
 
 SHOULD_LINT=0
 SHOULD_FIX=0
@@ -50,7 +56,19 @@ case $MODE in
         exit 1
 esac
 
-pushd ${PROJECT_DIR}
-[ $SHOULD_FIX -eq 1 ] && swift package plugin --allow-writing-to-package-directory format-source-code --configuration .swift-format.json ${TARGETS_ARGS}
-[ $SHOULD_LINT -eq 1 ] && swift package plugin lint-source-code --configuration .swift-format.json ${TARGETS_ARGS}
-popd
+# Check the swift-format executable
+SWIFT_FORMAT="${PROJECT_DIR}/.build/${SWIFT_FORMAT_BUILD_MODE}/swift-format"
+if [ ! -f ${SWIFT_FORMAT} ]; then
+    echo "Building: ${SWIFT_FORMAT}"
+    swift build -c ${SWIFT_FORMAT_BUILD_MODE} swift-format
+fi
+if [ ! -f ${SWIFT_FORMAT} ]; then
+    echo "Cannot build ${SWIFT_FORMAT}"
+    exit 1
+fi
+
+COMMON_ARGS="--configuration ${PROJECT_DIR}/.swift-format.json -r -p ${PROJECT_DIR}/Sources ${PROJECT_DIR}/Tests ${PROJECT_DIR}/*.swift"
+${SWIFT_FORMAT} -i ${COMMON_ARGS}
+
+[ $SHOULD_FIX -eq 1 ] && ${SWIFT_FORMAT} -i ${COMMON_ARGS} && echo "Formatted the source code."
+[ $SHOULD_LINT -eq 1 ] && ${SWIFT_FORMAT} lint -s ${COMMON_ARGS} && echo "Linted the source code."
