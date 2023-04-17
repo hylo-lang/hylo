7369db9e96d6193a071efd60b0f426e44dbc5bcc
Dimi Racordon <dimitri.racordon@gmail.com>
2023-04-17 06:51:30 -0400

    Create a script to install the support library

---
+++ Tools/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+CURRENT_DIR=$(dirname "$0")
+PROJECT_DIR=$( cd "${CURRENT_DIR}/../" ; pwd -P )
+BUILD_DIR=${PROJECT_DIR}/.build/support
+TARGET_DIR="${1:-/usr/local/lib/}"
+
+# Exit when any command fails.
+set -e
+
+# Compile the support library.
+mkdir -p ${BUILD_DIR}
+c++ -O2 -c -std=c++20 -o ${BUILD_DIR}/ValSupport.o ${PROJECT_DIR}/Sources/Support/src/io.cc
+ar -rv ${BUILD_DIR}/ValSupport.a ${BUILD_DIR}/ValSupport.o
+
+# Install the support library.
+mv ${BUILD_DIR}/ValSupport.a ${TARGET_DIR}/.
