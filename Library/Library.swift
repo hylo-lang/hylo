8b4a3e350206301f52f2fffc0110c92af4002541
Dimi Racordon <dimitri.racordon@gmail.com>
2023-04-17 06:46:41 -0400

    Add documentation

--- Library/Library.swift
+++ Library/Library.swift
@@ -1,5 +1,10 @@
 import class Foundation.Bundle
 
+/// The root URL of Val's core library.
 public let core = Bundle.module.url(forResource: "Core", withExtension: nil)
 
+/// The root URL of Val's support library.
+public let support = Bundle.module.url(forResource: "Core", withExtension: nil)
+
+/// The root URL of Val's support library for C++ transpiled sources.
 public let cxxSupport = Bundle.module.url(forResource: "CXX/Support", withExtension: nil)
