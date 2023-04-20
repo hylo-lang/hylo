f4bcdea8da9867205052750896a5a798c5d9fdda
Dimi Racordon <dimitri.racordon@gmail.com>
2023-04-20 08:45:23 -0400

    Refactor the representation of existential types

--- Sources/Core/Types/ExistentialType.swift
+++ Sources/Core/Types/ExistentialType.swift
@@ -3,8 +3,19 @@ import Utils
 /// An existential type, optionally bound by traits and constraints on associated types.
 public struct ExistentialType: TypeProtocol {
 
-  /// The traits to which the witness is known to conform.
-  public let traits: Set<TraitType>
+  /// The interface of an existential type.
+  public enum Interface: Hashable {
+
+    /// The traits to which the witness is known to conform.
+    case traits(Set<TraitType>)
+
+    /// The unparameterized generic type of the witness.
+    case generic(AnyType)
+
+  }
+
+  /// The interface of this type's instances.
+  public let interface: Interface
 
   /// The constraints on the associated types of the witness.
   ///
@@ -21,11 +32,27 @@ public struct ExistentialType: TypeProtocol {
         "type may only be constrained by equality or conformance")
     }
 
-    self.traits = traits
+    self.interface = .traits(traits)
     self.constraints = constraints
 
     // FIXME: Consider the types in the cosntraints?
     self.flags = traits.reduce(into: TypeFlags.isCanonical, { (a, b) in a.merge(b.flags) })
+  }
+
+  /// Creates a new existential type bound by an unparameterized generic type and constraints.
+  public init(unparameterized generic: AnyType, constraints: ConstraintSet) {
+    precondition(isValidInterface(generic))
+    for c in constraints {
+      precondition(
+        (c is EqualityConstraint) || (c is ConformanceConstraint),
+        "type may only be constrained by equality or conformance")
+    }
+
+    self.interface = .generic(generic)
+    self.constraints = constraints
+
+    // FIXME: Consider the types in the cosntraints?
+    self.flags = generic.flags
   }
 
 }
@@ -33,15 +60,35 @@ public struct ExistentialType: TypeProtocol {
 extension ExistentialType: CustomStringConvertible {
 
   public var description: String {
-    if traits.isEmpty && constraints.isEmpty { return "Any" }
+    let i: String
 
-    let t = traits.map({ "\($0)" }).joined(separator: " & ")
+    switch interface {
+    case .traits(let traits):
+      if traits.isEmpty && constraints.isEmpty {
+        return "Any"
+      } else {
+        i = "\(list: traits, joinedBy: " & ")"
+      }
+
+    case .generic(let t):
+      i = .init(describing: t)
+    }
+
     if constraints.isEmpty {
-      return "any \(t)"
+      return "any \(i)"
     } else {
-      let c = constraints.map({ "\($0)" }).joined(separator: ", ")
-      return "any \(t) where \(c)"
+      return "any \(i) where \(list: constraints)"
     }
   }
 
+}
+
+/// Returns `true` iff `t` is a valid interface for an existential type.
+private func isValidInterface(_ t: AnyType) -> Bool {
+  switch t.base {
+  case is ProductType, is TypeAliasType:
+    return t[.hasGenericTypeParameter] || t[.hasGenericValueParameter]
+  default:
+    return false
+  }
 }
