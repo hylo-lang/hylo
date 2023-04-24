1fc5fa4e82dc7857e46739cbc38ad05572389227
Cai Bingjun <1945458160@qq.com>
2023-04-24 22:07:26 +0800

    clean `install.bat`

--- Tools/install.bat
+++ Tools/install.bat
@@ -1,8 +1,8 @@
-# Compile the support library.
+:: Compile the support library.
 cd ../
 swift build -c release --target Support
 
-# Install the support library.
+:: Install the support library.
 cd %USERPROFILE%
 md .val
 cd .val
@@ -11,11 +11,7 @@ md lib
 :: Install the support library.
 cd %~dp0
 cd ../
-cd .build
-cd x86_64-unknown-windows-msvc
-cd release
-cd Support.build
-cd src
+cd .build/x86_64-unknown-windows-msvc/release/Support.build/src
 ren io.cc.o val_support.lib
 move val_support.lib %USERPROFILE%/.val/lib
 pause
