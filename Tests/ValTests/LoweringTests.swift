fa8b8320b5f6125eb40a54345bbbee71304ee74f
Dimi Racordon <dimitri.racordon@gmail.com>
2023-04-27 20:13:27 -0400

    Refactor loweing tests

--- Tests/ValTests/LoweringTests.swift
+++ Tests/ValTests/LoweringTests.swift
@@ -7,24 +7,25 @@ import XCTest
 final class LoweringTests: XCTestCase {
 
   func testLowering() throws {
-    try checkAnnotatedValFileDiagnostics(
-      inSuiteAt: "TestCases/Lowering",
-      { (source, diagnostics) in
-        // Note: built-in module is visible so that we can test built-in function calls.
-        var ast = AST.coreModule
-        let module = try ast.makeModule(
-          source.baseName, sourceCode: [source],
-          builtinModuleAccess: true, diagnostics: &diagnostics)
+    try checkAnnotatedValFileDiagnostics(inSuiteAt: "TestCases/Lowering") { (source, log) in
+      var ir = try lower(source, log: &log)
+      try ir.applyMandatoryPasses(reportingDiagnosticsInto: &log)
+    }
+  }
 
-        // Run the type checker
-        let typedProgram = try TypedProgram(ast, diagnostics: &diagnostics)
+  /// Returns the Val IR of `source`, reporting diagnostics to `log`.
+  private func lower(_ source: SourceFile, log: inout DiagnosticSet) throws -> IR.Module {
+    // Note: built-in module is visible so that we can test built-in function calls.
+    var ast = AST.coreModule
+    let module = try ast.makeModule(
+      source.baseName, sourceCode: [source],
+      builtinModuleAccess: true, diagnostics: &log)
 
-        // Emit Val's IR.
-        var irModule = try Module(lowering: module, in: typedProgram, diagnostics: &diagnostics)
+    // Run the type checker
+    let typedProgram = try TypedProgram(ast, diagnostics: &log)
 
-        // Run mandatory IR analysis and transformation passes.
-        try irModule.applyMandatoryPasses(reportingDiagnosticsInto: &diagnostics)
-      })
+    // Emit Val's IR.
+    return try Module(lowering: module, in: typedProgram, diagnostics: &log)
   }
 
 }
