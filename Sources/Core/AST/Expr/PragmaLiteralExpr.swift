52c15db07559d417d1cb8a1f2f11fc56fcb535c4
Dimi Racordon <dimitri.racordon@gmail.com>
2023-04-28 07:29:59 -0400

    Add an AST note to represent pragma literals

---
+++ Sources/Core/AST/Expr/PragmaLiteralExpr.swift
@@ -0,0 +1,24 @@
+/// A value injected by the compiler.
+public struct PragmaLiteralExpr: Expr {
+
+  /// The kind of a pragma literal.
+  public enum Kind: Codable {
+
+    /// The file in which the literal appears.
+    case file
+
+  }
+
+  /// The site from which `self` was parsed.
+  public let site: SourceRange
+
+  /// The kind of the literal.
+  public let kind: Kind
+
+  /// Creates a literal with given `kind` at `site.`
+  public init(_ kind: Kind, at site: SourceRange) {
+    self.site = site
+    self.kind = kind
+  }
+
+}
