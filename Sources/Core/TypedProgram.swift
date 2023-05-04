a72ca8da5b5537c72da25276cea165ef458da7bc
Dimi Racordon <dimitri.racordon@gmail.com>
2023-05-04 15:13:21 -0400

    Add helpers to gather lifted parameter declarations

--- Sources/Core/TypedProgram.swift
+++ Sources/Core/TypedProgram.swift
@@ -74,4 +74,78 @@ public struct TypedProgram: Program {
     self.relations = relations
   }
 
+  /// Returns `d`, which declares an explicit capture, paired with its type.
+  private func pairedWithType(
+    capture d: BindingDecl.ID
+  ) -> CallableParameterDecl {
+    switch ast[ast[d].pattern].introducer.value {
+    case .let:
+      return .init(decl: AnyDeclID(d), type: ParameterType(.let, declTypes[d]!))
+    case .inout:
+      return .init(decl: AnyDeclID(d), type: ParameterType(.inout, declTypes[d]!))
+    case .sinklet, .var:
+      return .init(decl: AnyDeclID(d), type: ParameterType(.sink, declTypes[d]!))
+    }
+  }
+
+  /// Returns `d`, which declares a parameter, paired with its type.
+  private func pairedWithType(
+    parameter d: ParameterDecl.ID
+  ) -> CallableParameterDecl {
+    .init(decl: AnyDeclID(d), type: ParameterType(declTypes[d]!)!)
+  }
+
+  /// Returns the declarations and types of the parameters of `d`'s lifted form.
+  ///
+  /// If `d` is a member function, its receiver comes first followed by formal parameters, from
+  /// left to right. Otherwise, its explicit captures come first, in the order they appear in its
+  /// capture list, from left to right. Implicit captures come next, in the order they were found
+  /// during type checking. Formal parameters come last, from left to right.
+  public func liftedParameters(of d: FunctionDecl.ID) -> [CallableParameterDecl] {
+    var result: [CallableParameterDecl] = []
+    if let r = ast[d].receiver {
+      result.append(pairedWithType(parameter: r))
+    } else {
+      result.append(contentsOf: ast[d].explicitCaptures.map(pairedWithType(capture:)))
+      result.append(contentsOf: implicitCaptures[d]!.map { (c) in
+        .init(decl: c.decl, type: ParameterType(c.type))
+      })
+    }
+    result.append(contentsOf: ast[d].parameters.map(pairedWithType(parameter:)))
+    return result
+  }
+
+  /// Returns the declarations of the parameters of `d`'s lifted form.
+  ///
+  /// `d`'s receiver comes first and is followed by `d`'s formal parameters, from left to right.
+  public func liftedParameters(of d: InitializerDecl.ID) -> [CallableParameterDecl] {
+    var result: [CallableParameterDecl] = []
+    result.append(pairedWithType(parameter: ast[d].receiver))
+    result.append(contentsOf: ast[d].parameters.map(pairedWithType(parameter:)))
+    return result
+  }
+
+  /// Returns the declarations of the parameters of `d`'s lifted form.
+  ///
+  /// If `d` is a member subscript, its receiver comes first followed by formal parameters, from
+  /// left to right. Otherwise, its explicit captures come first, in the order they appear in its
+  /// capture list, from left to right. Implicit captures come next, in the order they were found
+  /// during type checking. Formal parameters come last, from left to right.
+  public func liftedParameters(of d: SubscriptImpl.ID) -> [CallableParameterDecl] {
+    let bundle = SubscriptDecl.ID(declToScope[d]!)!
+    var result: [CallableParameterDecl] = []
+    if let r = ast[d].receiver {
+      result.append(pairedWithType(parameter: r))
+    } else {
+      result.append(contentsOf: ast[bundle].explicitCaptures.map(pairedWithType(capture:)))
+      result.append(contentsOf: implicitCaptures[bundle]!.map { (c) in
+        .init(decl: c.decl, type: ParameterType(c.type))
+      })
+    }
+    if let p = ast[bundle].parameters {
+      result.append(contentsOf: p.map(pairedWithType(parameter:)))
+    }
+    return result
+  }
+
 }
