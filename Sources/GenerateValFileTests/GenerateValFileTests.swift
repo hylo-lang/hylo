133442a5cbfe9db179a35e68cf333eb93195be23
Dave Abrahams <dabrahams@adobe.com>
2023-05-27 20:01:03 -0700

    Factor out first line parsing

--- Sources/GenerateValFileTests/GenerateValFileTests.swift
+++ Sources/GenerateValFileTests/GenerateValFileTests.swift
@@ -20,6 +20,8 @@ struct GenerateValFileTests: AsyncParsableCommand {
   )
   var valSourceFiles: [URL]
 
+  /// Returns a mapping from the elements of `valSourceFiles` to the first line of the file that
+  /// element refers to.
   func firstLineOfEachInput() async throws -> [URL: String] {
     try await withThrowingTaskGroup(of: (URL, String).self) { (g) -> [URL: String] in
       for f in valSourceFiles {
@@ -38,43 +40,15 @@ struct GenerateValFileTests: AsyncParsableCommand {
 
   /// Returns the Swift source of the test function for the Val at `source`, having `firstLine` as
   /// its first line.
-  func swiftFunctionTesting(valAt source: URL, withFirstLine firstLine: String) -> String {
+  func swiftFunctionTesting(valAt source: URL, withFirstLine firstLine: String) throws -> String {
 
-    func die(_ message: String) -> Never {
-      try! FileHandle.standardError.write(
-        contentsOf: Data("\(source.path):1: error: \(message)\n".utf8))
-      GenerateValFileTests.exit(withError: ExitCode(-1))
-    }
-
-    var text = firstLine[...]
-    if !text.removeLeading("//- ") {
-      die("first line of annotated test file must begin with “//-”.")
-    }
-    let methodName = text.removeUntil { $0.isWhitespace }
-    if methodName.isEmpty {
-      die("missing test method name.")
-    }
-    text = text.drop(while: \.isWhitespace)
-
-    if !text.removeLeading("expecting:") {
-      die("missing “expecting:” after test method name.")
-    }
-    text = text.drop(while: \.isWhitespace)
-
-    let expectation = text.removeUntil { $0.isWhitespace }
-    if expectation != "success" && expectation != "failure" {
-      die(
-        "illegal expectation “\(expectation)” must be “success” or “failure”."
-      )
-    }
-    if !text.drop(while: \.isWhitespace).isEmpty {
-      die("illegal trailing text “\(text)”.")
-    }
+    let parsed = try firstLine.parsedAsFirstLineOfAnnotatedValFileTest()
 
     return """
 
       func test_\(source.lastPathComponent.asSwiftIdentifier)() throws {
-        try \(methodName)(\(String(reflecting: source.path)), expectSuccess: \(expectation == "success"))
+        try \(parsed.methodName)(
+          \(String(reflecting: source.path)), expectSuccess: \(parsed.expectSuccess))
       }
 
       """
@@ -94,12 +68,19 @@ struct GenerateValFileTests: AsyncParsableCommand {
       """
 
     for f in valSourceFiles {
-      output += swiftFunctionTesting(valAt: f, withFirstLine: firstLines[f]!)
+      do {
+        output += try swiftFunctionTesting(valAt: f, withFirstLine: firstLines[f]!)
+      } catch let e as FirstLineError {
+        try! FileHandle.standardError.write(
+          contentsOf: Data("\(f.path):1: error: \(e.details)\n".utf8))
+        GenerateValFileTests.exit(withError: ExitCode(-1))
+      }
     }
 
     output += "\n}\n"
 
     try output.write(to: outputURL, atomically: true, encoding: .utf8)
+
   }
 }
 
@@ -141,4 +122,49 @@ extension Substring {
     return self[..<firstToKeep]
   }
 
+}
+
+extension String {
+
+  /// Interpreting `self` as the first line of an annotated Val test file, returns the embedded test
+  /// method name and expectation of success.
+  func parsedAsFirstLineOfAnnotatedValFileTest() throws -> (
+    methodName: Substring, expectSuccess: Bool
+  ) {
+    var text = self[...]
+    if !text.removeLeading("//- ") {
+      throw FirstLineError("first line of annotated test file must begin with “//-”.")
+    }
+    let methodName = text.removeUntil { $0.isWhitespace }
+    if methodName.isEmpty {
+      throw FirstLineError("missing test method name.")
+    }
+    text = text.drop(while: \.isWhitespace)
+
+    if !text.removeLeading("expecting:") {
+      throw FirstLineError("missing “expecting:” after test method name.")
+    }
+    text = text.drop(while: \.isWhitespace)
+
+    let expectation = text.removeUntil { $0.isWhitespace }
+    if expectation != "success" && expectation != "failure" {
+      throw FirstLineError(
+        "illegal expectation “\(expectation)” must be “success” or “failure”."
+      )
+    }
+    if !text.drop(while: \.isWhitespace).isEmpty {
+      throw FirstLineError("illegal trailing text “\(text)”.")
+    }
+    return (methodName: methodName, expectSuccess: expectation == "success")
+  }
+
+}
+
+/// A failure to parse the first line of an annotated Val file.
+struct FirstLineError: Error {
+  /// Creates an instance whose detailed description is `details`.
+  init(_ details: String) { self.details = details }
+
+  /// A detailed description of the cause of this failure.
+  let details: String
 }
