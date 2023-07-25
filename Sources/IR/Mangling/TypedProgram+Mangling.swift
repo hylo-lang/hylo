5adbd207ed2586d70b18151d7d593c4e39240de8
Dimi Racordon <dimitri.racordon@gmail.com>
2023-07-25 15:38:46 -0400

    Refactor mangling's public API

--- Sources/IR/Mangling/TypedProgram+Mangling.swift
+++ Sources/IR/Mangling/TypedProgram+Mangling.swift
@@ -4,17 +4,20 @@ extension TypedProgram {
 
   /// Returns the mangled representation of `d`.
   public func mangled<T: DeclID>(_ d: T) -> String {
-    var output = ""
-    var m = Mangler(self)
-    m.mangle(d, to: &output)
-    return output.assemblySanitized
+    mangled(d, applying: { (s, m, o) in m.mangle(decl: s, to: &o) })
   }
 
   /// Returns the mangled representation of `t`.
   public func mangled<T: TypeProtocol>(_ t: T) -> String {
+    mangled(^t, applying: { (s, m, o) in m.mangle(type: s, to: &o) })
+  }
+
+  private func mangled<T>(
+    _ symbol: T, applying mangle: (T, inout Mangler, inout String) -> ()
+  ) -> String {
     var output = ""
     var m = Mangler(self)
-    m.mangle(AnyType(t), to: &output)
+    mangle(symbol, &m, &output)
     return output.assemblySanitized
   }
 
