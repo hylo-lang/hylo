7bcb04ed0328f2a8f9410893b89cc4cd1e242b41
Dimi Racordon <dimitri.racordon@gmail.com>
2023-08-14 07:19:16 -0400

    Gather the requirements of capturing declarations in a protocol

--- Sources/Core/AST/Decl/SubscriptDecl.swift
+++ Sources/Core/AST/Decl/SubscriptDecl.swift
@@ -1,5 +1,5 @@
 /// A subscript declaration.
-public struct SubscriptDecl: BundleDecl, GenericDecl, GenericScope {
+public struct SubscriptDecl: BundleDecl, CapturingDecl, GenericDecl, GenericScope {
 
   public typealias Variant = SubscriptImpl
 
