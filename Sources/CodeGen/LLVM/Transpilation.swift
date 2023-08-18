342a3365b2ad84fa92911b03c04affea2c5cbc06
Dimi Racordon <dimitri.racordon@gmail.com>
2023-08-18 18:34:56 -0400

    Removed dead code

--- Sources/CodeGen/LLVM/Transpilation.swift
+++ Sources/CodeGen/LLVM/Transpilation.swift
@@ -192,10 +192,6 @@ extension LLVM.Module {
     case let v as TraitType:
       return transpiledTrait(v, usedIn: m, from: ir)
 
-    case is IR.Poison:
-      let t = ir.llvm(c.type.ast, in: &self)
-      return LLVM.Poison(of: t)
-
     case is IR.VoidConstant:
       return LLVM.StructConstant(aggregating: [], in: &self)
 
