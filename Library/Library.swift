8c595d7dcd7fa98f1cbff0ffbe1ab74c756fd44c
Dave Abrahams <dabrahams@adobe.com>
2023-09-11 14:42:32 -0700

    WIP

--- Library/Library.swift
+++ Library/Library.swift
@@ -1,9 +1,4 @@
 import Foundation
 
-fileprivate let libraryRoot = URL(fileURLWithPath: #filePath).deletingLastPathComponent()
-
-/// The root URL of Hylo's core library.
-public let core = libraryRoot.appendingPathComponent("Hylo/Core")
-
-/// The root URL of Hylo's standard library.
-public let standardLibrary = libraryRoot.appendingPathComponent("Hylo")
+public let standardLibraryRoot = URL(fileURLWithPath: #filePath).deletingLastPathComponent()
+public let standardLibraryBundleRoot = Bundle.module.url(forResource: "SourceGeneration", withExtension: nil)!
--- Sources/FrontEnd/AST+Extensions.swift
+++ Sources/FrontEnd/AST+Extensions.swift
@@ -6,19 +6,24 @@ import Utils
 extension AST {
 
   /// An instance that includes just the core module.
-  public static var coreModule = AST(libraryRoot: HyloModule.core)
+  public static var coreModule = AST(standardLibraryPathSuffix: "Hylo/Core")
 
   /// An instance that includes just the standard library.
-  public static var standardLibrary = AST(libraryRoot: HyloModule.standardLibrary)
+  public static var standardLibrary = AST(standardLibraryPathSuffix: "Hylo")
 
-  /// Creates an instance that includes the Hylo library rooted at `libraryRoot`.
-  private init(libraryRoot: URL) {
+  /// Creates an instance that includes the Hylo standard library components under the given path
+  /// suffix (both plain source and files generated from .gyb).
+  private init(standardLibraryPathSuffix: String) {
     self.init()
     do {
       var diagnostics = DiagnosticSet()
       coreLibrary = try makeModule(
         "Hylo",
-        sourceCode: sourceFiles(in: [libraryRoot]),
+        sourceCode: sourceFiles(
+          in: [
+            standardLibraryRoot.appendingPathComponent(standardLibraryPathSuffix),
+            standardLibraryBundleRoot.appendingPathComponent(standardLibraryPathSuffix)
+          ]),
         builtinModuleAccess: true,
         diagnostics: &diagnostics)
       assert(isCoreModuleLoaded)
