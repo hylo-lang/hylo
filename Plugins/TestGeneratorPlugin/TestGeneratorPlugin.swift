87dc481d150930881602cad15a7fa40fdd358d65
Dave Abrahams <dabrahams@adobe.com>
2023-09-24 16:05:47 -0700

    Try to make TestGeneratorPlugin portable

--- Plugins/TestGeneratorPlugin/TestGeneratorPlugin.swift
+++ Plugins/TestGeneratorPlugin/TestGeneratorPlugin.swift
@@ -3,20 +3,24 @@ import PackagePlugin
 /// The Swift Package Manager plugin that generates XCTest cases for annotated ".hylo" files as
 /// part of our build process.
 @main
-struct TestGeneratorPlugin: BuildToolPlugin {
+struct TestGeneratorPlugin: PortableBuildToolPlugin {
 
-  func createBuildCommands(context: PluginContext, target: Target) throws -> [Command] {
+  func portableBuildCommands(
+    context: PackagePlugin.PluginContext, target: PackagePlugin.Target
+  ) throws -> [PortableBuildCommand]
+  {
     guard let target = target as? SourceModuleTarget else { return [] }
-    let inputPaths = target.sourceFiles(withSuffix: "hylo").map(\.path)
+    let inputPaths = target.sourceFiles(withSuffix: ".hylo").map(\.path)
     let outputPath = context.pluginWorkDirectory.appending("HyloFileTests.swift")
 
-    let cmd = Command.buildCommand(
+    return [
+      .buildCommand(
       displayName: "Generating XCTestCases into \(outputPath)",
-      executable: try context.tool(named: "GenerateHyloFileTests").path,
-      arguments: ["-o", outputPath, "-n", target.moduleName] + inputPaths,
+      tool: .executableProduct(name: "GenerateHyloFileTests"),
+      arguments: ["-o", outputPath.platformString, "-n", target.moduleName]
+        + inputPaths.map(\.platformString),
       inputFiles: inputPaths,
-      outputFiles: [outputPath])
-    return [cmd]
+      outputFiles: [outputPath])]
   }
 
 }
