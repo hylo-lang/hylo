f619e5fd9a80e97d4163f0547b12f10ebc6004ca
Dimi Racordon <dimitri.racordon@gmail.com>
2023-09-25 18:30:13 +0200

    Move 'TypedProgram.accumulatedGenericParameters' to the type checker

--- Sources/FrontEnd/TypeChecking/TypeChecker.swift
+++ Sources/FrontEnd/TypeChecking/TypeChecker.swift
@@ -3295,6 +3295,61 @@ struct TypeChecker {
     return result
   }
 
+  /// Returns generic parameters captured by `s` and the scopes semantically containing `s`.
+  ///
+  /// A declaration may take generic parameters even if it doesn't declare any. For example, a
+  /// nested function will implicitly capture the generic parameters introduced in its context.
+  ///
+  /// Parameters are returned outer to inner, left to right: the first parameter of the outermost
+  /// generic scope appears first; the last parameter of the innermost generic scope appears last.
+  public mutating func accumulatedGenericParameters<T: ScopeID>(
+    in s: T
+  ) -> ReversedCollection<[GenericParameterDecl.ID]> {
+    var result: [GenericParameterDecl.ID] = []
+    appendGenericParameters(in: s, to: &result)
+    return result.reversed()
+  }
+
+  /// Appends generic parameters captured by `s` and the scopes semantically containing `s` to
+  /// `accumulatedParameters`, right to left, inner to outer.
+  private mutating func appendGenericParameters<T: ScopeID>(
+    in s: T, to accumulatedParameters: inout [GenericParameterDecl.ID]
+  ) {
+    switch s.kind.value {
+    case is ConformanceDecl.Type:
+      appendGenericParameters(in: ConformanceDecl.ID(s)!, to: &accumulatedParameters)
+    case is ExtensionDecl.Type:
+      appendGenericParameters(in: ExtensionDecl.ID(s)!, to: &accumulatedParameters)
+    case is GenericScope.Type:
+      accumulatedParameters.append(contentsOf: (program.ast[s] as! GenericScope).genericParameters)
+    case is TranslationUnit.Type, is ModuleDecl.Type:
+      return
+    default:
+      break
+    }
+
+    appendGenericParameters(in: program[s].scope, to: &accumulatedParameters)
+  }
+
+  /// Appends generic parameters captured by `s` and the scopes semantically containing `s` to
+  /// `accumulatedParameters`, right to left, inner to outer.
+  private mutating func appendGenericParameters<T: TypeExtendingDecl>(
+    in d: T.ID, to accumulatedParameters: inout [GenericParameterDecl.ID]
+  ) {
+    guard let p = scopeExtended(by: d) else { return }
+    appendGenericParameters(in: p, to: &accumulatedParameters)
+  }
+
+  /// Appends generic parameters captured by `s` and the scopes semantically containing `s` to
+  /// `accumulatedParameters`, right to left, inner to outer.
+  private func appendGenericParameters<T: GenericDecl>(
+    in d: T.ID, to accumulatedParameters: inout [GenericParameterDecl.ID]
+  ) {
+    if let clause = program[d].genericClause {
+      accumulatedParameters.append(contentsOf: clause.value.parameters)
+    }
+  }
+
   // MARK: Quantifier elimination
 
   /// A context in which a generic parameter can be instantiated.
