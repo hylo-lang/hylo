76a57bea07dc11e33da3a4b27d81f8fdd759a79b
Dave Abrahams <dabrahams@adobe.com>
2023-10-12 02:57:57 -0700

    Nix nonportable autoreleasepool
    Shouldn't be needed anyway.

--- Sources/Utils/URL+Extensions.swift
+++ Sources/Utils/URL+Extensions.swift
@@ -6,12 +6,10 @@ extension URL {
   ///
   /// - Precondition: `self` is a file scheme or file reference URL.
   public var fileSystemPath: String {
-    autoreleasepool {
-      self.standardizedFileURL.withUnsafeFileSystemRepresentation { (name: UnsafePointer<CChar>?) in
-        FileManager().string(
-          withFileSystemRepresentation: name!,
-          length: (0...).first(where: { i in name![i] == 0 })!)
-      }
+    self.standardizedFileURL.withUnsafeFileSystemRepresentation { (name: UnsafePointer<CChar>?) in
+      FileManager().string(
+        withFileSystemRepresentation: name!,
+        length: (0...).first(where: { i in name![i] == 0 })!)
     }
   }
 
