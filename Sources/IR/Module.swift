84d609f7281d34e6282636f913a76aed99a2014a
Dimi Racordon <dimitri.racordon@gmail.com>
2023-10-16 20:59:59 +0200

    Improve docs

--- Sources/IR/Module.swift
+++ Sources/IR/Module.swift
@@ -6,8 +6,9 @@ import Utils
 
 /// A module lowered to Hylo IR.
 ///
-/// An IR module is notionally composed of a collection of functions, one of which may be
-/// designated as its entry point (i.e., the `main` function of a Hylo program).
+/// A lowered module is a collection of IR functions and a collection of constant IR values, which
+/// represent nominal types, traits, and global bindings. These entities may not necessarily have
+/// a definition. When they don't, they denote a declaration known to be defined in another module.
 public struct Module {
 
   /// The identity of a global defined in a Hylo IR module.
@@ -38,6 +39,10 @@ public struct Module {
   public private(set) var synthesizedDecls = OrderedSet<SynthesizedFunctionDecl>()
 
   /// The module's entry function, if any.
+  ///
+  /// An entry function is the lowered form of a program's entry point, that is the `main` function
+  /// of a Hylo program. A module with an entry function is called an entry module. There can be
+  /// only one entry module in a program.
   public private(set) var entryFunction: Function.ID?
 
   /// Creates an instance lowering `m` in `p`, reporting errors and warnings to `log`.
