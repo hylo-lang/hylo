91f80ecf4292715e282a5ef82db8a0056add3946
Dave Abrahams <dabrahams@adobe.com>
2023-11-14 14:32:13 -0800

    Switch to throwing AST construction
    This is more likely to result in useful error reports.

--- StandardLibrary/AST+Extensions.swift
+++ StandardLibrary/AST+Extensions.swift
@@ -6,20 +6,16 @@ import Utils
 extension AST {
 
   /// Creates an instance that includes the Hylo library whose sources are rooted at `libraryRoot`.
-  init(libraryRoot: URL, for compiler: CompilerConfiguration) {
+  init(libraryRoot: URL, for compiler: CompilerConfiguration) throws {
     self.init(for: compiler)
-    do {
-      var diagnostics = DiagnosticSet()
-      coreLibrary = try makeModule(
-        "Hylo",
-        sourceCode: sourceFiles(in: [libraryRoot]),
-        builtinModuleAccess: true,
-        diagnostics: &diagnostics)
-      assert(isCoreModuleLoaded)
-      self.coreTraits = .init(self)
-    } catch let error {
-      fatalError("Error parsing the Hylo module:\n\(error)")
-    }
+    var diagnostics = DiagnosticSet()
+    coreLibrary = try makeModule(
+      "Hylo",
+      sourceCode: sourceFiles(in: [libraryRoot]),
+      builtinModuleAccess: true,
+      diagnostics: &diagnostics)
+    assert(isCoreModuleLoaded)
+    self.coreTraits = .init(self)
   }
 
 }
