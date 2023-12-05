b6f6946f3857a9fcdca29b16a109cb4a1ebef148
Dave Abrahams <dabrahams@adobe.com>
2023-12-05 15:45:03 -0800

    Improve HostPlatform pattern so things don't need to be redeclared.

--- Sources/Utils/HostPlatform.swift
+++ Sources/Utils/HostPlatform.swift
@@ -1,17 +1,28 @@
+/// The platform on which the compiler or interpreter is running.
 public enum HostPlatform {
-  #if os(Windows)
-    /// The name of the environment variable containing the executable search path.
-    public static let pathEnvironmentVariable = "Path"
-    /// The separator between elements of the executable search path.
-    public static let pathEnvironmentSeparator: Character = ";"
-    /// The file extension applied to binary executables.
-    public static let executableSuffix = ".exe"
-  #else
-    /// The name of the environment variable containing the executable search path.
-    public static let pathEnvironmentVariable = "PATH"
-    /// The separator between elements of the executable search path.
-    public static let pathEnvironmentSeparator: Character = ":"
-    /// The file extension applied to binary executables.
-    public static let executableSuffix = ""
+
+  /// An operating system on which the compiler or interpreter can run.
+  public enum OperatingSystem {
+    case macOS, linux, windows
+  }
+
+  #if os(macOS)
+  /// The operating system.
+  public static let operatingSystem: OperatingSystem = .macOS
+  #elseif os(Linux)
+  /// The operating system.
+  public static let operatingSystem: OperatingSystem = .linux
+  #elseif os(Windows)
+  /// The operating system.
+  public static let operatingSystem: OperatingSystem = .windows
   #endif
+
+  /// The name of the environment variable containing the executable search path.
+  public static let pathEnvironmentVariable = operatingSystem == .windows ? "Path" : "PATH"
+
+  /// The separator between elements of the environment's executable search path.
+  public static let pathEnvironmentSeparator: Character = operatingSystem == .windows ? ";" : ":"
+
+  /// The file extension applied to binary executables.
+  public static let executableSuffix = operatingSystem == .windows ? ".exe" : ""
 }
