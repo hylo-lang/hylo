13e232c756cc5e38ded359780ad970785846f101
Dave Abrahams <dabrahams@adobe.com>
2023-12-08 10:33:55 -0800

    Eliminate subscript access on SharedMutable (#1177)

--- Sources/FrontEnd/TypeChecking/TypeChecker.swift
+++ Sources/FrontEnd/TypeChecking/TypeChecker.swift
@@ -58,7 +58,8 @@ struct TypeChecker {
   ) {
     self.identifier = identifier
     self.nextFreshVariableIdentifier = UInt64(identifier) << 56
-    self.cache = Cache(local: instanceUnderConstruction[], shared: instanceUnderConstruction)
+    self.cache = instanceUnderConstruction.read(
+      applying: { Cache(local: $0, shared: instanceUnderConstruction) })
     self.shouldTraceInference = shouldTraceInference
   }
 
@@ -5560,12 +5561,14 @@ struct TypeChecker {
     ) -> V? {
       if let v = local[keyPath: path] {
         return v
-      } else if !ignoreSharedCache, let v = shared?[][keyPath: path] {
-        local[keyPath: path] = v
-        return v
-      } else {
-        return nil
       }
+      if ignoreSharedCache { return nil }
+
+      guard let v = shared?.read(applying: { $0[keyPath: path] })
+      else { return nil }
+
+      local[keyPath: path] = v
+      return v
     }
 
     /// Writes `value` to the cache at `path`, applying the update with `merge`.
