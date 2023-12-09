06da4c22612c35cabd9d549e7ad6120be0f9ad02
Dave Abrahams <dabrahams@adobe.com>
2023-12-08 17:59:06 -0800

    Add missing file

---
+++ Sources/Utils/ResultOrError.swift
@@ -0,0 +1,42 @@
+/// A value of type `T` or the error produced by computing that value.
+public enum ResultOrError<T> {
+
+  /// The successful computation.
+  case result(T)
+
+  /// The error thrown by a failed computation.
+  case error(any Error)
+
+  /// The result of `constructResult()` or if it throws, the error it threw.
+  public init(_ constructResult: () throws -> T) {
+    do {
+      self = .result(try constructResult())
+    } catch let e {
+      self = .error(e)
+    }
+  }
+
+  /// The result of the succesful computation if any, or `nil` otherwise.
+  public var result: T? {
+    if case .result(let r) = self { return r }
+    return nil
+  }
+
+  /// The error thrown by the failed computation if any, or `nil` otherwise.
+  public var error: (any Error)? {
+    if case .error(let e) = self { return e }
+    return nil
+  }
+
+  /// Accesses the result of the successful computation if any, or else rethrows the error thrown by
+  /// the computation.
+  public subscript() -> T {
+    get throws {
+      switch self {
+      case .result(let r): return r
+      case .error(let e): throw e
+      }
+    }
+  }
+
+}
