7e0a9ea9f3757a00f01ed5573bce9ee8bfe0903e
Dimi Racordon <dimitri.racordon@gmail.com>
2024-01-08 22:02:43 +0100

    Allow any type to appear in the domain of associated types

--- Sources/Core/Types/AnyType.swift
+++ Sources/Core/Types/AnyType.swift
@@ -189,7 +189,9 @@ public struct AnyType {
   /// Returns `true` iff `self` is bound to an existential quantifier.
   public var isSkolem: Bool {
     switch base {
-    case is AssociatedTypeType, is GenericTypeParameterType:
+    case let u as AssociatedTypeType:
+      return u.domain.isSkolem
+    case is GenericTypeParameterType:
       return true
     case let u as ConformanceLensType:
       return u.subject.isSkolem
--- Sources/Core/Types/AssociatedTypeType.swift
+++ Sources/Core/Types/AssociatedTypeType.swift
@@ -14,27 +14,33 @@ public struct AssociatedTypeType: TypeProtocol {
   /// The name of the associated type.
   public let name: Incidental<String>
 
+  /// A set of flags describing recursive properties.
+  public let flags: TypeFlags
+
   /// Creates an instance denoting the associated type declared by `decl` as a member of `domain`.
-  ///
-  /// - Requires: `domain` is an associated type, conformance lens, or generic type parameter.
   public init(_ decl: AssociatedTypeDecl.ID, domain: AnyType, ast: AST) {
-    switch domain.base {
-    case is AssociatedTypeType, is ConformanceLensType, is GenericTypeParameterType:
-      self.domain = domain
-    default:
-      preconditionFailure("invalid associated type domain")
+    var fs = domain.flags
+    if !domain.isSkolem && !(domain.base is TypeVariable) {
+      fs.remove(.isCanonical)
     }
 
+    self.domain = domain
     self.decl = decl
     self.name = Incidental(ast[decl].baseName)
+    self.flags = fs
   }
 
-  public var flags: TypeFlags { .isCanonical }
 
 }
 
 extension AssociatedTypeType: CustomStringConvertible {
 
-  public var description: String { "\(domain).\(name.value)" }
+  public var description: String {
+    if domain.base is ConformanceLensType {
+      return "(\(domain)).\(name.value)"
+    } else {
+      return "\(domain).\(name.value)"
+    }
+  }
 
 }
