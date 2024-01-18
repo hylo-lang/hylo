2012b8f6f78816511f31259be3561437de5eae23
Dimi Racordon <dimitri.racordon@gmail.com>
2024-01-18 14:42:20 +0100

    Fix type visitation in associated types

--- Sources/Core/Types/AssociatedTypeType.swift
+++ Sources/Core/Types/AssociatedTypeType.swift
@@ -19,17 +19,28 @@ public struct AssociatedTypeType: TypeProtocol {
 
   /// Creates an instance denoting the associated type declared by `decl` as a member of `domain`.
   public init(_ decl: AssociatedTypeDecl.ID, domain: AnyType, ast: AST) {
+    self.init(decl: decl, domain: domain, name: ast[decl].baseName)
+  }
+
+  /// Creates an instance with the given properties.
+  init(decl: AssociatedTypeDecl.ID, domain: AnyType, name: String) {
     var fs = domain.flags
     if !domain.isSkolem && !(domain.base is TypeVariable) {
       fs.remove(.isCanonical)
     }
 
-    self.domain = domain
     self.decl = decl
-    self.name = Incidental(ast[decl].baseName)
+    self.domain = domain
+    self.name = Incidental(name)
     self.flags = fs
   }
 
+  public func transformParts<M>(
+    mutating m: inout M, _ transformer: (inout M, AnyType) -> TypeTransformAction
+  ) -> Self {
+    let d = domain.transform(mutating: &m, transformer)
+    return AssociatedTypeType(decl: decl, domain: d, name: name.value)
+  }
 
 }
 
