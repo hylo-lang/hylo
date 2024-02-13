66dfafb53384723a72e91997017bb6a225602040
Dimi Racordon <dimitri.racordon@gmail.com>
2024-02-13 22:22:38 +0100

    Add a property to project parts containing implicit captures

--- Sources/Core/AST/Decl/CapturingDecl.swift
+++ Sources/Core/AST/Decl/CapturingDecl.swift
@@ -4,4 +4,7 @@ public protocol CapturingDecl: Decl, LexicalScope {
   /// The explicit capture declarations of the entity.
   var explicitCaptures: [BindingDecl.ID] { get }
 
+  /// The part of the declaration that may have implicit captures.
+  var sourcesOfImplicitCaptures: [AnyNodeID] { get }
+
 }
