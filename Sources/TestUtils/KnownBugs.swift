64a9b1a887e89cdcf956d874d0739126e103acbf
Dave Abrahams <dabrahams@adobe.com>
2024-03-06 07:29:47 -0800

    See if we've gotten past the Windows mandatory pass crash.

--- Sources/TestUtils/KnownBugs.swift
+++ Sources/TestUtils/KnownBugs.swift
@@ -1,6 +1,2 @@
 /// Whether Swifty-LLVM crashes when running mandatory passes: (https://github.com/hylo-lang/Swifty-LLVM/issues/24).
-#if os(Windows)
-  public let swiftyLLVMMandatoryPassesCrash = true
-#else
-  public let swiftyLLVMMandatoryPassesCrash = false
-#endif
+public let swiftyLLVMMandatoryPassesCrash = false
