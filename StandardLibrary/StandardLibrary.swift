fc58ee92df0567a695eb8ea67cd2002e53189b38
Tóth Ambrus <32463042+tothambrus11@users.noreply.github.com>
2024-07-02 14:05:30 +0000

    fix cmake builds

--- StandardLibrary/StandardLibrary.swift
+++ StandardLibrary/StandardLibrary.swift
@@ -2,20 +2,26 @@ import Foundation
 import FrontEnd
 import Utils
 
+private let standardLibrarySourceFolder = URL(fileURLWithPath: #filePath).deletingLastPathComponent().appendingPathComponent("Sources")
+#if canImport(SPMUtility) // Check if SPM is being used
+  /// The parent directory of the standard library sources directory.
+  private let useBuiltinStandardLibrary = ProcessInfo.processInfo.environment["HYLO_USE_BUILTIN_STDLIB"] == "true"
 
-/// The parent directory of the standard library sources directory.
-private let useBuiltinStandardLibrary = ProcessInfo.processInfo.environment["HYLO_USE_BUILTIN_STDLIB"] == "true"
+  private let libraryRoot = if useBuiltinStandardLibrary {
+    // This path points into the source tree rather than to some copy so that when diagnostics are
+    // issued for the standard library, they point to the original source files and edits to those
+    // source files actually fix the problems (see https://github.com/hylo-lang/hylo/issues/932).  If we
+    // ever change that, some other mechanism is needed to ensure that diagnostics refer to the original
+    // source files.
+    standardLibrarySourceFolder
+  } else {
+    Bundle.module.url(forResource: "Sources", withExtension: nil)!
+  }
+#else
+  // Default to source location on SPM. TODO make this able to use exported resources
+  private let libraryRoot = standardLibrarySourceFolder
+#endif
 
-private let libraryRoot = if useBuiltinStandardLibrary {
-  // This path points into the source tree rather than to some copy so that when diagnostics are
-  // issued for the standard library, they point to the original source files and edits to those
-  // source files actually fix the problems (see https://github.com/hylo-lang/hylo/issues/932).  If we
-  // ever change that, some other mechanism is needed to ensure that diagnostics refer to the original
-  // source files.
-  URL(fileURLWithPath: #filePath).appendingPathComponent("Sources")
-} else {
-  Bundle.module.url(forResource: "Sources", withExtension: nil)!
-}
 
 /// The root of a directory hierarchy containing all the standard library sources.
 private let hostedLibrarySourceRoot = libraryRoot
