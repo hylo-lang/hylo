3f3aaa54c829a5fa93adc0add4f68661885c5aaf
Dimi Racordon <dimitri.racordon@gmail.com>
2024-08-17 02:26:42 +0200

    Rename obsolete reference to Val in parameter names

--- Sources/CodeGen/LLVM/TypeLowering.swift
+++ Sources/CodeGen/LLVM/TypeLowering.swift
@@ -5,33 +5,33 @@ import Utils
 
 extension IR.Program {
 
-  /// Returns the LLVM form of `val` in `module`.
+  /// Returns the LLVM form of `t` in `module`.
   ///
-  /// - Requires: `val` is representable in LLVM.
-  func llvm<T: TypeProtocol>(_ val: T, in module: inout SwiftyLLVM.Module) -> SwiftyLLVM.IRType {
-    switch val {
-    case let t as AnyType:
-      return llvm(t.base, in: &module)
-    case let t as ArrowType:
-      return llvm(arrowType: t, in: &module)
-    case let t as BufferType:
-      return llvm(bufferType: t, in: &module)
-    case let t as BuiltinType:
-      return llvm(builtinType: t, in: &module)
-    case let t as BoundGenericType:
-      return llvm(boundGenericType: t, in: &module)
+  /// - Requires: `t` is representable in LLVM.
+  func llvm<T: TypeProtocol>(_ t: T, in module: inout SwiftyLLVM.Module) -> SwiftyLLVM.IRType {
+    switch t {
+    case let u as AnyType:
+      return llvm(u.base, in: &module)
+    case let u as ArrowType:
+      return llvm(arrowType: u, in: &module)
+    case let u as BufferType:
+      return llvm(bufferType: u, in: &module)
+    case let u as BuiltinType:
+      return llvm(builtinType: u, in: &module)
+    case let u as BoundGenericType:
+      return llvm(boundGenericType: u, in: &module)
     case is MetatypeType:
       return module.ptr
-    case let t as ProductType:
-      return llvm(productType: t, in: &module)
+    case let u as ProductType:
+      return llvm(productType: u, in: &module)
     case is RemoteType:
       return module.ptr
-    case let t as TupleType:
-      return llvm(tupleType: t, in: &module)
-    case let t as UnionType:
-      return llvm(unionType: t, in: &module)
+    case let u as TupleType:
+      return llvm(tupleType: u, in: &module)
+    case let u as UnionType:
+      return llvm(unionType: u, in: &module)
     default:
-      notLLVMRepresentable(val)
+      notLLVMRepresentable(t)
     }
   }
 
@@ -44,22 +44,22 @@ extension IR.Program {
     return SwiftyLLVM.StructType([module.ptr, e], in: &module)
   }
 
-  /// Returns the LLVM form of `val` in `module`.
+  /// Returns the LLVM form of `t` in `module`.
   ///
-  /// - Requires: `val` is representable in LLVM.
-  func llvm(bufferType val: BufferType, in module: inout SwiftyLLVM.Module) -> SwiftyLLVM.IRType {
-    let e = llvm(val.element, in: &module)
-    guard let n = val.count.asCompilerKnown(Int.self) else {
-      notLLVMRepresentable(val)
+  /// - Requires: `t` is representable in LLVM.
+  func llvm(bufferType t: BufferType, in module: inout SwiftyLLVM.Module) -> SwiftyLLVM.IRType {
+    let e = llvm(t.element, in: &module)
+    guard let n = t.count.asCompilerKnown(Int.self) else {
+      notLLVMRepresentable(t)
     }
     return SwiftyLLVM.ArrayType(n, e, in: &module)
   }
 
-  /// Returns the LLVM form of `val` in `module`.
+  /// Returns the LLVM form of `t` in `module`.
   ///
-  /// - Requires: `val` is representable in LLVM.
-  func llvm(builtinType val: BuiltinType, in module: inout SwiftyLLVM.Module) -> SwiftyLLVM.IRType {
-    switch val {
+  /// - Requires: `t` is representable in LLVM.
+  func llvm(builtinType t: BuiltinType, in module: inout SwiftyLLVM.Module) -> SwiftyLLVM.IRType {
+    switch t {
     case .i(let width):
       return SwiftyLLVM.IntegerType(width, in: &module)
     case .word:
@@ -75,21 +75,21 @@ extension IR.Program {
     case .ptr:
       return module.ptr
     case .module:
-      notLLVMRepresentable(val)
+      notLLVMRepresentable(t)
     }
   }
 
-  /// Returns the LLVM form of `val` in `module`.
+  /// Returns the LLVM form of `t` in `module`.
   ///
-  /// - Requires: `val` is representable in LLVM.
+  /// - Requires: `t` is representable in LLVM.
   func llvm(
-    boundGenericType val: BoundGenericType, in module: inout SwiftyLLVM.Module
+    boundGenericType t: BoundGenericType, in module: inout SwiftyLLVM.Module
   ) -> SwiftyLLVM.IRType {
-    precondition(val[.isCanonical])
+    precondition(t[.isCanonical])
 
-    let fields = base.storage(of: val).map({ (p) in llvm(p.type, in: &module) })
+    let fields = base.storage(of: t).map({ (p) in llvm(p.type, in: &module) })
 
-    switch val.base.base {
+    switch t.base.base {
     case let u as ProductType:
       return SwiftyLLVM.StructType(named: u.name.value, fields, in: &module)
     case is TupleType:
@@ -99,19 +99,19 @@ extension IR.Program {
     }
   }
 
-  /// Returns the LLVM form of `val` in `module`.
+  /// Returns the LLVM form of `t` in `module`.
   ///
-  /// - Requires: `val` is representable in LLVM.
-  func llvm(productType val: ProductType, in module: inout SwiftyLLVM.Module) -> SwiftyLLVM.IRType {
-    precondition(val[.isCanonical])
+  /// - Requires: `t` is representable in LLVM.
+  func llvm(productType t: ProductType, in module: inout SwiftyLLVM.Module) -> SwiftyLLVM.IRType {
+    precondition(t[.isCanonical])
 
-    let n = base.mangled(val)
-    if let t = module.type(named: n) {
-      assert(SwiftyLLVM.StructType(t) != nil)
-      return t
+    let n = base.mangled(t)
+    if let u = module.type(named: n) {
+      assert(SwiftyLLVM.StructType(u) != nil)
+      return u
     }
 
-    let l = AbstractTypeLayout(of: val, definedIn: base)
+    let l = AbstractTypeLayout(of: t, definedIn: base)
     var fields: [SwiftyLLVM.IRType] = []
     for p in l.properties {
       fields.append(llvm(p.type, in: &module))
@@ -120,35 +120,35 @@ extension IR.Program {
     return SwiftyLLVM.StructType(named: n, fields, in: &module)
   }
 
-  /// Returns the LLVM form of `val` in `module`.
+  /// Returns the LLVM form of `t` in `module`.
   ///
-  /// - Requires: `val` is representable in LLVM.
-  func llvm(tupleType val: TupleType, in module: inout SwiftyLLVM.Module) -> SwiftyLLVM.IRType {
-    precondition(val[.isCanonical])
+  /// - Requires: `t` is representable in LLVM.
+  func llvm(tupleType t: TupleType, in module: inout SwiftyLLVM.Module) -> SwiftyLLVM.IRType {
+    precondition(t[.isCanonical])
 
     var fields: [SwiftyLLVM.IRType] = []
-    for e in val.elements {
+    for e in t.elements {
       fields.append(llvm(e.type, in: &module))
     }
 
     return SwiftyLLVM.StructType(fields, in: &module)
   }
 
-  /// Returns the LLVM form of `val` in `module`.
+  /// Returns the LLVM form of `t` in `module`.
   ///
-  /// - Requires: `val` is representable in LLVM.
-  func llvm(unionType val: UnionType, in module: inout SwiftyLLVM.Module) -> SwiftyLLVM.IRType {
-    precondition(val[.isCanonical])
+  /// - Requires: `t` is representable in LLVM.
+  func llvm(unionType t: UnionType, in module: inout SwiftyLLVM.Module) -> SwiftyLLVM.IRType {
+    precondition(t[.isCanonical])
 
     var payload: SwiftyLLVM.IRType = SwiftyLLVM.StructType([], in: &module)
-    if val.isNever {
+    if t.isNever {
       return payload
     }
 
-    for e in val.elements {
-      let t = llvm(e, in: &module)
-      if module.layout.storageSize(of: t) > module.layout.storageSize(of: payload) {
-        payload = t
+    for e in t.elements {
+      let u = llvm(e, in: &module)
+      if module.layout.storageSize(of: u) > module.layout.storageSize(of: payload) {
+        payload = u
       }
     }
     return StructType([payload, module.word()], in: &module)
