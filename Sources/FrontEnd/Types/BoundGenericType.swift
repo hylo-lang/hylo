14656c6dbaf4eb994e61b7c689f9c37e3c46c131
Dimi Racordon <dimitri.racordon@gmail.com>
2024-08-26 22:49:02 +0200

    Merge term flags in the representation of bound generic types

--- Sources/FrontEnd/Types/BoundGenericType.swift
+++ Sources/FrontEnd/Types/BoundGenericType.swift
@@ -23,7 +23,7 @@ public struct BoundGenericType: TypeProtocol {
     self.arguments = arguments
 
     var flags = arguments.reduce(base.flags) { (fs, a) in
-      fs | (a.value.asType ?? UNIMPLEMENTED()).flags
+      fs | a.value.flags
     }
 
     // The type isn't canonical if `base` is structural.
