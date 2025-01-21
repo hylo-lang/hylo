892c0c4b389a156a934142afcb1e3b53f6f63813
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2025-01-22 00:08:41 +0200

    Add `atomic_store_N` calls to the test C program.

--- ci-test/atomics_test.c
+++ ci-test/atomics_test.c
@@ -10,5 +10,11 @@ int main() {
     printf("i16 atomic free: %d\n", __atomic_always_lock_free(sizeof(i16), &i16));
     printf("i32 atomic free: %d\n", __atomic_always_lock_free(sizeof(i32), &i32));
     printf("i64 atomic free: %d\n", __atomic_always_lock_free(sizeof(i64), &i64));
+
+    __atomic_store_1(&i8, 0, 0);
+    __atomic_store_2(&i16, 0, 0);
+    __atomic_store_4(&i32, 0, 0);
+    __atomic_store_8(&i64, 0, 0);
+
     return 0;
 }
