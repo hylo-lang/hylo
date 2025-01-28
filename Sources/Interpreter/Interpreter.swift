424187dae5835a46e9d1ed2495da59efce44fa5f
Dave Abrahams <dabrahams@adobe.com>
2025-01-27 17:37:00 -0800

    WIP

--- Sources/Interpreter/Interpreter.swift
+++ Sources/Interpreter/Interpreter.swift
@@ -1,2 +1,32 @@
-/// Nothing to see here.
-struct Interpreter {}
+import IR
+
+struct CodePointer {
+
+  var module: Module.ID
+  var instructionInModule: InstructionID
+
+}
+
+struct Register {
+  let type: Type
+}
+
+struct StackFrame {
+
+}
+
+/// A virtual machine that executes Hylo's in-memory IR representation.
+struct Interpreter {
+
+  /// The program to be executed.
+  private let program: Program
+
+  /// An instance executing `p`.
+  public init(_ p: Program) {
+    program = p
+  }
+
+  /// Executes a single instruction.
+  public func step() throws {}
+
+}
