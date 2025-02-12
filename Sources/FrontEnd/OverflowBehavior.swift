20d9e818aeeb25ed393cc79ebb39d4c545023a0b
Dave Abrahams <dabrahams@adobe.com>
2025-02-11 17:50:02 -0800

    Decouple FrontEnd from Swifty-LLVM

---
+++ Sources/FrontEnd/OverflowBehavior.swift
@@ -0,0 +1,13 @@
+/// The result of overflow during mathematical operations.
+public enum OverflowBehavior {
+
+  /// Overflow is ignored.
+  case ignore
+
+  /// The result is a poison value should unsigned overflow occur.
+  case nuw
+
+  /// The result is a poison value should signed overflow occur.
+  case nsw
+
+}
