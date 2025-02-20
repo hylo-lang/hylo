a25401700f297562296d26cd4f0e8ea8144d462d
Dave Abrahams <dabrahams@adobe.com>
2025-02-19 16:32:24 -0800

    Narrow/simplify precondition; improve efficiency.

--- Sources/Utils/BinaryInteger+Extensions.swift
+++ Sources/Utils/BinaryInteger+Extensions.swift
@@ -2,12 +2,12 @@ extension BinaryInteger {
 
   /// Returns `self` rounded up to the nearest multiple of `n`.
   ///
-  /// - Precondition: `n` is nonzero
-  /// - Precondition: `self` is negative, or `self` + |n| - 1 is representable as `Self`.
+  /// - Precondition: `n` is nonzero and |`n`| is representable as `Self`.
   public func rounded(upToNearestMultipleOf n: Self) -> Self {
     precondition(n != 0)
     let m = Self(n.magnitude)
-    return (self < 0 ? self : self + (m - 1)) / m * m
+    let r = self % m
+    return (self < 0 ? self - r : self + (m - r))
   }
 
 }
