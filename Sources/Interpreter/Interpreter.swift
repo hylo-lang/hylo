d1015ee539f30e0c38a1fc46fc7393028beabb09
Dave Abrahams <dabrahams@adobe.com>
2025-08-04 13:53:51 -0700

    Interpreter WIP

--- Sources/Interpreter/Interpreter.swift
+++ Sources/Interpreter/Interpreter.swift
@@ -9,6 +9,7 @@ struct CodePointer {
 
 }
 
+/// The result value of an instruction.
 struct Register {
 
   let type: IR.`Type`
@@ -16,10 +17,16 @@ struct Register {
 
 }
 
+/// The local variables, parameters, and return address for a function
+/// call.
 struct StackFrame {
 
+  /// The results of instructions.
   var registers: Array<Register>
-  var programCounter: CodePointer
+
+  /// The program counter to which execution should return when
+  /// popping this frame.
+  var returnAddress: CodePointer
 
 }
 
@@ -29,12 +36,147 @@ struct Interpreter {
   /// The program to be executed.
   private let program: IR.Program
 
+  /// The stack- and dynamically-allocated memory in use by the program.
+  private var memory = Heap()
+
+  /// Local variables, parameters, and return addresses.
+  private var stack: [StackFrame]
+
+  /// Identity of the next instruction to be executed.
+  private var programCounter: CodePointer
+
   /// An instance executing `p`.
+  ///
+  /// - Precondition: `p.entry != nil`
   public init(_ p: IR.Program) {
     program = p
+    let entryModuleID = p.entry!
+    let entryModule = p.modules[entryModuleID]!
+    let entryFunctionID = entryModule.entryFunction!
+    let entryFunction = entryModule.functions[entryFunctionID]!
+    let entryBlockID = entryFunction.entry!
+    let entryInstructionAddress = entryFunction.blocks[entryBlockID].instructions.firstAddress!
+    programCounter = .init(
+      module: entryModuleID,
+      instructionInModule: InstructionID(entryFunctionID, entryBlockID, entryInstructionAddress))
+
+    // The return address of the bottom-most frame will never be used,
+    // so we fill it with something arbitrary.
+    stack = [StackFrame(registers: [], returnAddress: programCounter)]
   }
 
   /// Executes a single instruction.
-  public func step() throws {}
+  public mutating func step() throws {
+    switch currentInstruction {
+    case is Access:
+      // No effect on program state
+      break
+    case let x as AddressToPointer:
+      _ = x
+    case let x as AdvancedByBytes:
+      _ = x
+    case let x as AdvancedByStrides:
+      _ = x
+    case let x as AllocStack:
+      _ = x
+    case let x as Branch:
+      _ = x
+    case let x as Call:
+      _ = x
+    case let x as CallBuiltinFunction:
+      _ = x
+    case is CallBundle:
+      fatalError("Interpreter: CallBundle instructions have not been removed.")
+    case let x as CallFFI:
+      _ = x
+    case let x as CaptureIn:
+      _ = x
+    case let x as CloseCapture:
+      _ = x
+    case let x as CloseUnion:
+      _ = x
+    case let x as CondBranch:
+      _ = x
+    case let x as ConstantString:
+      _ = x
+    case let x as DeallocStack:
+      _ = x
+    case is EndAccess:
+      // No effect on program state
+      break
+    case let x as EndProject:
+      _ = x
+    case let x as GenericParameter:
+      _ = x
+    case let x as GlobalAddr:
+      _ = x
+    case let x as Load:
+      _ = x
+    case is MarkState:
+      // No effect on program state
+      break
+    case let x as MemoryCopy:
+      _ = x
+    case is Move:
+      fatalError("Interpreter: Move instructions have not been removed.")
+    case let x as OpenCapture:
+      _ = x
+    case let x as OpenUnion:
+      _ = x
+    case let x as PointerToAddress:
+      _ = x
+    case let x as Project:
+      _ = x
+    case is ProjectBundle:
+      fatalError("Interpreter: ProjectBundle instructions have not been removed.")
+    case is ReleaseCaptures:
+      // No effect on program state
+      break
+    case let x as Return:
+      _ = x
+    case let x as Store:
+      _ = x
+    case let x as SubfieldView:
+      _ = x
+    case let x as Switch:
+      _ = x
+    case let x as UnionDiscriminator:
+      _ = x
+    case let x as UnionSwitch:
+      _ = x
+    case let x as Unreachable:
+      _ = x
+    case let x as WrapExistentialAddr:
+      _ = x
+    case let x as Yield:
+      _ = x
+    default:
+      fatalError("Interpreter: unimplemented instruction")
+    }
+
+    advanceProgramCounter()
+  }
+
+  /// The instruction at which the program counter points.
+  ///
+  /// - Precondition: the program is running.
+  public var currentInstruction: any Instruction {
+    _read {
+      yield program.modules[programCounter.module]!
+      .functions[programCounter.instructionInModule.function]!
+      .blocks[programCounter.instructionInModule.block][programCounter.instructionInModule.address]
+    }
+  }
+
+  /// Moves the program counter to the next instruction.
+  mutating func advanceProgramCounter() {
+    let b = program.modules[programCounter.module]!
+      .functions[programCounter.instructionInModule.function]!
+      .blocks[programCounter.instructionInModule.block].instructions
+    programCounter.instructionInModule = InstructionID(
+      programCounter.instructionInModule.function,
+      programCounter.instructionInModule.block,
+      b.address(after: programCounter.instructionInModule.address)!)
+  }
 
 }
