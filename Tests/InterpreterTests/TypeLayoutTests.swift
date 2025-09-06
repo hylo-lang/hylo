8e1ae3b3e5da5ab1fc35fd1c7d80c900c42f4fe7
Dave Abrahams <dabrahams@adobe.com>
2025-09-05 20:32:54 -0700

    Test trivial tuple layout

--- Tests/InterpreterTests/TypeLayoutTests.swift
+++ Tests/InterpreterTests/TypeLayoutTests.swift
@@ -16,22 +16,37 @@ final class TypeLayoutTests: XCTestCase {
     let i8 = c[^BuiltinType.i(8)]
     XCTAssertEqual(i8.size, 1)
     XCTAssertEqual(i8.alignment, 1)
-    XCTAssertEqual(i8.components.count, 0)
+    XCTAssert(i8.components.isEmpty)
 
     let i16 = c[^BuiltinType.i(16)]
     XCTAssertEqual(i16.size, 2)
     XCTAssertEqual(i16.alignment, 2)
-    XCTAssertEqual(i16.components.count, 0)
+    XCTAssert(i16.components.isEmpty)
 
     let i32 = c[^BuiltinType.i(32)]
     XCTAssertEqual(i32.size, 4)
     XCTAssertEqual(i32.alignment, 4)
-    XCTAssertEqual(i32.components.count, 0)
+    XCTAssert(i32.components.isEmpty)
 
     let i64 = c[^BuiltinType.i(64)]
     XCTAssertEqual(i64.size, 8)
     XCTAssertEqual(i64.alignment, 8)
-    XCTAssertEqual(i64.components.count, 0)
+    XCTAssert(i64.components.isEmpty)
+  }
+
+  func testTrivialTuples() throws {
+    var c = TypeLayoutCache(typesIn: emptyProgram, for: UnrealABI())
+
+    let void = c[.void]
+    XCTAssertEqual(void.size, 0)
+    XCTAssertEqual(void.alignment, 1)
+    XCTAssert(void.components.isEmpty)
+
+
+    let justI8 = c[^TupleType(types: [^BuiltinType.i(8)])]
+    let i8 = c[^BuiltinType.i(8)]
+    XCTAssertEqual(justI8.bytes, i8.bytes)
+    XCTAssertEqual(justI8.components, [.init(name: "0", type: ^BuiltinType.i(8), offset: 0)])
   }
 
 }
