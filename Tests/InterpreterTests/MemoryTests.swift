94174e273bc598534a8556e5899881e301d7f674
Dave Abrahams <dabrahams@adobe.com>
2025-09-09 17:51:48 -0700

    Begin testing memory allocation/deallocation.

---
+++ Tests/InterpreterTests/MemoryTests.swift
@@ -0,0 +1,22 @@
+import FrontEnd
+import Testing
+
+@testable import Interpreter
+
+@Test func InterpreterMemory_allocation() {
+  var m = Memory()
+  var allocations: [Memory.Address] = []
+  for alignmentPower in 0..<8 {
+    let alignment = 1 << alignmentPower
+    let p = m.allocate(3, bytesWithAlignment: alignment)
+    allocations.append(p)
+    #expect(m.allocation[p.allocation]!.size == 3, "alignment \(alignment)")
+    #expect(m.address(p, hasAlignment: alignment))
+  }
+
+  for p in allocations {
+    m.deallocate(p)
+  }
+
+  let p = allocations[0]
+}
