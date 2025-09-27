98e1137c252f1517f6578a437d694f6e392ba7cb
Ambrus Tóth <ping@ambrus.dev>
2025-09-27 11:15:27 +0300

    remove windows known bug

--- Sources/TestUtils/KnownBugs.swift
+++ Sources/TestUtils/KnownBugs.swift
@@ -1,6 +1,6 @@
 /// Whether Swifty-LLVM crashes when running mandatory passes: (https://github.com/hylo-lang/Swifty-LLVM/issues/24).
 #if os(Windows)
-  public let swiftyLLVMMandatoryPassesCrash = true
+  public let swiftyLLVMMandatoryPassesCrash = false
 #else
   public let swiftyLLVMMandatoryPassesCrash = false
 #endif
