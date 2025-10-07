cca23a257db3e983cf1cfe644e6b967daf10936d
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2025-10-07 22:55:37 +0300

    Remove `Block.AbsoluteID`
    Replace its use in Emitter with a simpler structure.

--- Sources/IR/BlockID.swift
+++ Sources/IR/BlockID.swift
@@ -13,11 +13,6 @@ extension Block {
       self.address = address
     }
 
-    /// Creates an instance with the given address.
-    public init(_ b: Block.AbsoluteID) {
-      self.address = b.address
-    }
-
     /// Creates an instance denoting the block containing `i`.
     public init(containing i: InstructionID) {
       self.address = i.block
@@ -41,49 +36,9 @@ extension Block {
 
   }
 
-  /// The absolute ID of a basic block.
-  public struct AbsoluteID: Hashable {
-
-    /// The ID of the function containing the block.
-    public var function: Function.ID
-
-    /// The address of the block in the containing function.
-    public var address: Function.Blocks.Address
-
-    /// Creates an instance with the given properties.
-    public init(_ function: Function.ID, _ address: Function.Blocks.Address) {
-      self.function = function
-      self.address = address
-    }
-
-    /// Creates an instance with the given properties.
-    public init(_ function: Function.ID, _ block: Block.ID) {
-      self.function = function
-      self.address = block.address
-    }
-
-    /// The ID of the `index`-th parameter of the block.
-    public func parameter(_ index: Int) -> Operand {
-      .parameter(Block.ID(self), index)
-    }
-
-    /// The operand denoting the result of the instruction at `instructionAddress` in the block
-    /// identified by `self`.
-    public func result(at instructionAddress: Block.Instructions.Address) -> Operand {
-      .register(InstructionID(address, instructionAddress))
-    }
-
-  }
-
 }
 
 extension Block.ID: CustomStringConvertible {
-
-  public var description: String { "b\(address)" }
-
-}
-
-extension Block.AbsoluteID: CustomStringConvertible {
 
   public var description: String { "b\(address)" }
 
