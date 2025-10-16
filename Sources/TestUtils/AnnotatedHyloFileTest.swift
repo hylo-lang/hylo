92279826226dfb0fc3e2eed1d24731991d992439
Dave Abrahams <dabrahams@adobe.com>
2025-10-15 17:25:56 -0700

    Use new facilities some.

--- Sources/TestUtils/AnnotatedHyloFileTest.swift
+++ Sources/TestUtils/AnnotatedHyloFileTest.swift
@@ -249,19 +249,8 @@ extension XCTestCase {
     try checkAnnotatedHyloFileDiagnostics(
       inFileAt: hyloFilePath, expecting: expectation
     ) { (hyloSource, log) in
-
-      let (p, m) = try p.loadModule(reportingDiagnosticsTo: &log) { (ast, log, space) in
-        // Note: built-in module is visible so that we can test built-in function calls.
-        try ast.loadModule(
-          hyloSource.baseName, parsing: [hyloSource], inNodeSpace: space,
-          withBuiltinModuleAccess: true,
-          reportingDiagnosticsTo: &log)
-      }
-
-      // Emit Hylo IR.
-      var ir = try Module(lowering: m, in: p, reportingDiagnosticsTo: &log)
-      // Run mandatory IR analysis and transformation passes.
-      try ir.applyMandatoryPasses(reportingDiagnosticsTo: &log)
+      _ = try hyloSource.typecheckedWithStandardLibrary(reportingDiagnosticsTo: &log, withBuiltinModuleAccess: true)
+        .latestModuleIR(reportingDiagnosticsTo: &log)
     }
   }
 
--- Sources/TestUtils/ProgramInstances.swift
+++ Sources/TestUtils/ProgramInstances.swift
@@ -37,9 +37,12 @@ extension SourceFile {
   }
 
   /// Returns `self` as a one-file module, lowered to IR.
-  public func loweredToIR() throws -> IR.Module {
+  ///
+  /// - Parameter `needsBuiltins`: `true` iff `self` should be allowed access to
+  ///   builtin functions.
+  public func loweredToIR(withBuiltinModuleAccess needsBuiltins: Bool = false) throws -> IR.Module {
     var log = DiagnosticSet()
-    return try self.typecheckedWithStandardLibrary(reportingDiagnosticsTo: &log)
+    return try self.typecheckedWithStandardLibrary(reportingDiagnosticsTo: &log, withBuiltinModuleAccess: needsBuiltins)
       .latestModuleIR(reportingDiagnosticsTo: &log)
   }
 
@@ -47,13 +50,17 @@ extension SourceFile {
   /// typechecked.
   ///
   /// The module for `self` is the result's `latestModule`.
+  ///
+  /// - Parameter `needsBuiltins`: `true` iff `self` should be allowed access to
+  ///   builtin functions.
   public func typecheckedWithStandardLibrary(
-    reportingDiagnosticsTo log: inout DiagnosticSet
+    reportingDiagnosticsTo log: inout DiagnosticSet,
+    withBuiltinModuleAccess needsBuiltins: Bool = false
   ) throws -> TypedProgram {
     log.formUnion(typecheckedStandardLibrary.diagnostics)
     return try typecheckedStandardLibrary.program
       .loadModule(reportingDiagnosticsTo: &log) { ast, log, nodeSpace in
-        try ast.loadModule("Main", parsing: [self], reportingDiagnosticsTo: &log)
+        try ast.loadModule("Main", parsing: [self], withBuiltinModuleAccess: needsBuiltins, reportingDiagnosticsTo: &log)
       }.0
   }
 
@@ -62,16 +69,16 @@ extension SourceFile {
 extension AST {
 
   /// The latest module loaded.
-  public var latestModule: ModuleDecl.ID {
-    return modules.last!
-  }
+  public var latestModule: ModuleDecl.ID { modules.last! }
 
 }
 
 extension TypedProgram {
 
-  /// Returns the last-loaded module lowered to IR, or throws diagnostics.
-  func latestModuleIR(reportingDiagnosticsTo log: inout DiagnosticSet) throws -> IR.Module {
+  public var latestModule: ModuleDecl.ID { base.ast.latestModule }
+
+  /// Returns the last-loaded module, lowered to IR.
+  public func latestModuleIR(reportingDiagnosticsTo log: inout DiagnosticSet) throws -> IR.Module {
     var r = try IR.Module(lowering: base.ast.latestModule, in: self, reportingDiagnosticsTo: &log)
     try r.applyMandatoryPasses(reportingDiagnosticsTo: &log)
     return r
