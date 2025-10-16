603cd11b8446351e1b9cc5b356e94ab3ecc605ab
Ambrus Tóth <ping@ambrus.dev>
2025-10-16 13:24:00 +0000

    remove known bugs

--- Sources/TestUtils/KnownBugs.swift
+++
@@ -1,6 +0,0 @@
-/// Whether Swifty-LLVM crashes when running mandatory passes: (https://github.com/hylo-lang/Swifty-LLVM/issues/24).
-#if os(Windows)
-  public let swiftyLLVMMandatoryPassesCrash = false
-#else
-  public let swiftyLLVMMandatoryPassesCrash = false
-#endif
