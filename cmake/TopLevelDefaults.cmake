c0b79cd16fae717d3b159a660684de948621c2dd
Ambrus Tóth <ping@ambrus.dev>
2025-10-18 23:06:53 +0300

    Update CMakeModules

--- cmake/TopLevelDefaults.cmake
+++ cmake/TopLevelDefaults.cmake
@@ -14,7 +14,7 @@ block()
   set(FETCHCONTENT_TRY_FIND_PACKAGE_MODE NEVER)
   FetchContent_Declare(Hylo-CMakeModules
     GIT_REPOSITORY https://github.com/hylo-lang/CMakeModules.git
-    GIT_TAG        fix-numerics
+    GIT_TAG        9e5cc47ec31a4ab2ad30c7aff6102c433b32c31f
     OVERRIDE_FIND_PACKAGE
   )
 
