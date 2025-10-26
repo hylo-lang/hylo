181ee9509ea73845b7ad5ea3283e91fb10673f85
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2025-10-26 20:59:44 +0530

    executor(for: ) method would be more generic

--- Tests/InterpreterTests/InterpreterTests.swift
+++ Tests/InterpreterTests/InterpreterTests.swift
@@ -1,32 +1,28 @@
 import Driver
 import Foundation
 import IR
-import Interpreter
+import Testing
 import Utils
-import XCTest
 
-final class InterpreterTests: XCTestCase {
+@testable import Interpreter
 
-  func run(_ program: String) throws -> Interpreter {
+@Suite struct InterpreterRunTests {
+
+  func executor(for program: String) throws -> Interpreter {
     let source = try FileManager.default.temporaryFile(containing: program)
 
     let compilation = try Driver.compileToTemporary(
       source, withOptions: ["--last-phase=lowering"])
     try compilation.diagnostics.throwOnError()
 
-    var executor = Interpreter(compilation.ir!)
-
-    func runProgram() throws {
-      while executor.isRunning { try executor.step() }
-    }
-
-    XCTAssertNoThrow(try runProgram())
-
-    return executor
+    return Interpreter(compilation.ir!)
   }
 
-  func testEmptyProgram() throws {
-    _ = try run(#"public fun main() {  }"#)
+  @Test func emptyProgramShouldWork() throws {
+    var executor = try executor(for: #"public fun main() {  }"#)
+    #expect(throws: Never.self) {
+      while executor.isRunning { try executor.step() }
+    }
   }
 
 }
