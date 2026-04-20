afd9d8bc921b9489fbe01f6bda19fc0e8590f3a9
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2026-04-20 18:22:25 +0530

    add more tests based on implementation detail

--- Tests/InterpreterTests/AccessTrackerTests.swift
+++ Tests/InterpreterTests/AccessTrackerTests.swift
@@ -70,6 +70,10 @@ final class AccessTrackerTests: XCTestCase {
         try t.requireIsActive(a2, at: ["b", "c"])
       }
       try t.requireIsActive(a3, at: ["b", "c"])
+      // sibling should be active.
+      let a4 = try t.begin(p, at: ["b", "d"])
+      try t.requireIsActive(a4, at: ["b", "d"])
+      try t.requireIsActive(a3, at: ["b", "c"])
     }
 
     var t = AccessTracker("a", with: .let)
@@ -82,40 +86,44 @@ final class AccessTrackerTests: XCTestCase {
     try t.requireIsActive(a1, at: ["b"])
     try t.requireIsActive(a2, at: ["b", "c"])
     try t.requireIsActive(a3, at: ["b", "c"])
+    // sibling should be active.
+    let a4 = try t.begin(.let, at: ["b", "d"])
+    try t.requireIsActive(a4, at: ["b", "d"])
+    try t.requireIsActive(a3, at: ["b", "c"])
   }
 
   func testEndAccess() throws {
     for p in [.inout, .sink, .set] as [AccessKind] {
       var t = AccessTracker("a", with: p)
-      let a1 = try t.begin(p, at: ["b"])
-      let a2 = try t.begin(p, at: ["b", "c"])
-      let a3 = try t.begin(p, at: ["b", "c"])
+      let a1 = try t.begin(p, at: ["b", "c"])
+      let a2 = try t.begin(p, at: ["b", "c", "d"])
+      let a3 = try t.begin(p, at: ["b", "c", "d"])
+      check(throws: Error.overlappingExclusiveAccessExists(for: "d")) {
+        try t.end(a2, at: ["b", "c", "d"])
+      }
       check(throws: Error.overlappingExclusiveAccessExists(for: "c")) {
-        try t.end(a2, at: ["b", "c"])
+        try t.end(a1, at: ["b", "c"])
       }
-      check(throws: Error.overlappingExclusiveAccessExists(for: "b")) {
-        try t.end(a1, at: ["b"])
-      }
-      try t.end(a3, at: ["b", "c"])
-      try t.end(a2, at: ["b", "c"])
+      try t.end(a3, at: ["b", "c", "d"])
+      try t.end(a2, at: ["b", "c", "d"])
 
-      let a4 = try t.begin(p, at: ["b"])  // can start new access after no overlapping subpart
-      try t.end(a4, at: ["b"])
-      try t.end(a1, at: ["b"])
+      let a4 = try t.begin(p, at: ["b", "c"])  // can start new access after no overlapping subpart
+      try t.end(a4, at: ["b", "c"])
+      try t.end(a1, at: ["b", "c"])
       let a5 = try t.begin(p, at: [])  // can start new access after no overlapping subpart
       try t.end(a5, at: [])
     }
 
     // Let access allows ending access in arbitrary order as all let access are always active.
     var t = AccessTracker("a", with: .let)
-    let a1 = try t.begin(.let, at: ["b"])
-    let a2 = try t.begin(.let, at: ["b", "c"])
-    let a3 = try t.begin(.let, at: ["b", "c"])
-    try t.end(a1, at: ["b"])
-    try t.end(a2, at: ["b", "c"])
-    try t.end(a3, at: ["b", "c"])
-    let a4 = try t.begin(.inout, at: ["b"])  // can start new access after no overlapping subpart
-    try t.end(a4, at: ["b"])
+    let a1 = try t.begin(.let, at: ["b", "c"])
+    let a2 = try t.begin(.let, at: ["b", "c", "d"])
+    let a3 = try t.begin(.let, at: ["b", "c", "d"])
+    try t.end(a1, at: ["b", "c"])
+    try t.end(a2, at: ["b", "c", "d"])
+    try t.end(a3, at: ["b", "c", "d"])
+    let a4 = try t.begin(.inout, at: ["b", "c"])  // can start new access after no overlapping subpart
+    try t.end(a4, at: ["b", "c"])
     let a5 = try t.begin(.inout, at: [])  // can start new access after no overlapping subpart
     try t.end(a5, at: [])
   }
